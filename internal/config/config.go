8ddcf31ea4fcb7bb977dcb7d202700ffc7d0b9c2
Joe Wang <joewa@google.com>
2025-07-09 15:56:41 -0400

    add comment

--- internal/config/config.go
+++ internal/config/config.go
@@ -28,7 +28,8 @@ const (
 	// GeneratorInputDir is the default directory to store files that generator
 	// needs to regenerate libraries from an empty directory.
 	GeneratorInputDir string = "generator-input"
-	GenerateRequest   string = "generate-request.json"
+	// GenerateRequest is a JSON file that describes which library to generate.
+	GenerateRequest string = "generate-request.json"
 	// LibrarianDir is the default directory to store librarian state/config files,
 	// along with any additional configuration.
 	LibrarianDir string = ".librarian"
