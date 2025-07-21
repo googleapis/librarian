c2668db3d2019db61b30488d798f444dfec59d43
Joe Wang <joewa@google.com>
2025-07-21 13:48:12 -0400

    remove test repo

--- internal/librarian/generate_e2e_test.go
+++ internal/librarian/generate_e2e_test.go
@@ -19,18 +19,25 @@ package librarian
 
 import (
 	"context"
+	"github.com/go-git/go-billy/v5/osfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/cache"
 	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/storage/filesystem"
 	"github.com/googleapis/librarian/internal/config"
+	"io"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
+)
 
-	"github.com/go-git/go-git/v5"
+const (
+	localRepoDir       = "../../testdata/e2e/generate/repo"
+	localRepoBackupDir = "../../testdata/e2e/generate/repo_backup"
 )
 
 func TestRunGenerate(t *testing.T) {
-	const (
-		localRepoDir = "../../testdata/e2e/generate/repo"
-	)
 	for _, test := range []struct {
 		name string
 		cfg  *config.Config
@@ -46,26 +53,39 @@ func TestRunGenerate(t *testing.T) {
 	} {
 		t.Run(test.name, func(t *testing.T) {
 			if err := initTestRepo(localRepoDir); err != nil {
-				t.Errorf("init test repo error = %v", err)
+				t.Fatalf("init test repo error = %v", err)
 			}
 			runner, err := newGenerateRunner(test.cfg)
 			if err != nil {
-				t.Errorf("newGenerateRunner() error = %v", err)
+				t.Fatalf("newGenerateRunner() error = %v", err)
 			}
 			if err := runner.run(context.Background()); err != nil {
-				t.Errorf("run() error = %v", err)
+				t.Fatalf("run() error = %v", err)
 			}
+
+			// test repo cleanup
+			os.RemoveAll(localRepoDir)
 		})
 	}
 }
 
 func initTestRepo(dir string) error {
-	localRepo, err := git.PlainInit(dir, false)
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+	storage := filesystem.NewStorage(osfs.New(filepath.Join(dir, ".git")), cache.NewObjectLRU(256))
+	worktree := osfs.New(dir)
+	localRepo, err := git.Init(storage, worktree)
 	if err != nil {
 		return err
 	}
+
 	workTree, err := localRepo.Worktree()
 	if err != nil {
+		return err
+	}
+	if err := copyDir(localRepoBackupDir, localRepoDir); err != nil {
 		return err
 	}
 	if _, err := workTree.Add("."); err != nil {
@@ -79,6 +99,76 @@ func initTestRepo(dir string) error {
 			When:  time.Now(),
 		},
 	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// copyDir recursively copies a directory from src to dest.
+func copyDir(src string, dest string) error {
+	srcInfo, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	if _, err := os.Stat(dest); os.IsNotExist(err) {
+		if err := os.MkdirAll(dest, srcInfo.Mode()); err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+
+	entries, err := os.ReadDir(src)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		destPath := filepath.Join(dest, entry.Name())
+
+		if entry.IsDir() {
+			// If it's a directory, recursively call CopyDir
+			if err := copyDir(srcPath, destPath); err != nil {
+				return err
+			}
+		} else {
+			// If it's a file, copy the file
+			if err := copyFile(srcPath, destPath); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// copyFile copies a single file from src to dest.
+func copyFile(src, dest string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	// Copy the file contents
+	if _, err := io.Copy(destFile, srcFile); err != nil {
+		return err
+	}
+
+	// Get source file info to copy permissions
+	srcInfo, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	// Set destination file permissions
+	if err := os.Chmod(dest, srcInfo.Mode()); err != nil {
 		return err
 	}
 
