7a8daf45b229eb4aa87c4431d3173eb0f9e5786e
JoeWang1127 <joewa@google.com>
2025-07-21 19:32:24 +0000

    add validation

--- testdata/e2e_func.go
+++ testdata/e2e_func.go
@@ -1,14 +1,62 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	inputDir  = "input"
+	librarian = "librarian"
+	libraryID = "library-id"
+	outputDir = "output"
+	source    = "source"
 )
 
 func main() {
-	if len(os.Args) > 1 {
-		fmt.Println("Command-line arguments:", os.Args[1:])
-	} else {
-		fmt.Println("No command-line arguments provided.")
+	if len(os.Args) <= 1 {
+		log.Fatal(errors.New("no command-line arguments provided"))
 	}
+
+	log.Print("received command: ", os.Args[1:])
+	switch os.Args[1] {
+	case "generate":
+		if err := validateGenerate(os.Args[2:]); err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal("unrecognized command: ", os.Args[1])
+	}
+}
+
+func validateGenerate(args []string) error {
+	for _, arg := range args {
+		arg, _ := strings.CutPrefix(arg, "--")
+		strs := strings.Split(arg, "=")
+		switch strs[0] {
+		case librarian:
+			if err := validateLibrarianDir(strs[1]); err != nil {
+				return err
+			}
+		case inputDir:
+			return nil
+		case outputDir:
+			return nil
+		case source:
+			return nil
+		}
+	}
+
+	return nil
+}
+
+func validateLibrarianDir(dir string) error {
+	if _, err := os.Stat(filepath.Join(dir, "generate-request.json")); err != nil {
+		return err
+	}
+
+	return nil
 }
