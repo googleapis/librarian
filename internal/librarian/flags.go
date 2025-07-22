e7e18642e9a49a13395bf9a1f366a5dd84e7c5b9
Min Zhu <zhumin@google.com>
2025-07-22 16:37:35 -0400

    add library flag

--- internal/librarian/flags.go
+++ internal/librarian/flags.go
@@ -34,18 +34,22 @@ func addFlagHostMount(fs *flag.FlagSet, cfg *config.Config) {
 	fs.StringVar(&cfg.HostMount, "host-mount", defaultValue, "a mount point from Docker host and within the Docker. The format is {host-dir}:{local-dir}.")
 }
 
-func addFlagPushConfig(fs *flag.FlagSet, cfg *config.Config) {
-	// TODO(https://github.com/googleapis/librarian/issues/724):remove the default for push-config
-	defaultValue := "noreply-cloudsdk@google.com,Google Cloud SDK"
-	fs.StringVar(&cfg.PushConfig, "push-config", defaultValue, "If specified, will try to create a commit and pull request for the generated changes. The format should be \"{git-email-address},{author-name}\". Also, when this field is specified it is expected a Github token will be provided with push access via the environment variable LIBRARIAN_GITHUB_TOKEN")
-}
-
 func addFlagImage(fs *flag.FlagSet, cfg *config.Config) {
 	fs.StringVar(&cfg.Image, "image", "", "Container image to run for subcommands. Defaults to the image in the pipeline state.")
 }
 
+func addFlagLibrary(fs *flag.FlagSet, cfg *config.Config) {
+	fs.StringVar(&cfg.Library, "library", "", "The ID of a single library to update")
+}
+
 func addFlagProject(fs *flag.FlagSet, cfg *config.Config) {
 	fs.StringVar(&cfg.Project, "project", "", "Project containing Secret Manager secrets.")
+}
+
+func addFlagPushConfig(fs *flag.FlagSet, cfg *config.Config) {
+	// TODO(https://github.com/googleapis/librarian/issues/724):remove the default for push-config
+	defaultValue := "noreply-cloudsdk@google.com,Google Cloud SDK"
+	fs.StringVar(&cfg.PushConfig, "push-config", defaultValue, "If specified, will try to create a commit and pull request for the generated changes. The format should be \"{git-email-address},{author-name}\". Also, when this field is specified it is expected a Github token will be provided with push access via the environment variable LIBRARIAN_GITHUB_TOKEN")
 }
 
 func addFlagRepo(fs *flag.FlagSet, cfg *config.Config) {
