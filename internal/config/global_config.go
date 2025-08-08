f2a5402570823bf6c969b484d1fb2dde1d431f7a
Joe Wang <joewa@google.com>
2025-08-08 10:43:07 -0400

    feat: add a global configuration

---
+++ internal/config/global_config.go
@@ -0,0 +1,49 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import "fmt"
+
+// GlobalConfig defines the contract for the config.yaml file.
+type GlobalConfig struct {
+	GlobalFilesAllowlist []*GlobalFile `yaml:"global_files_allowlist"`
+}
+
+// GlobalFile defines the global files in language repositories.
+type GlobalFile struct {
+	Path        string     `yaml:"path"`
+	Permissions Permission `yaml:"permissions"`
+}
+
+type Permission int
+
+const (
+	ReadOnly Permission = iota
+	WriteOnly
+	ReadWrite
+)
+
+func (permission Permission) String() string {
+	switch permission {
+	case ReadOnly:
+		return "ReadOnly"
+	case WriteOnly:
+		return "WriteOnly"
+	case ReadWrite:
+		return "ReadWrite"
+	default:
+		return fmt.Sprintf("Invalid Status: %d", permission)
+	}
+}
