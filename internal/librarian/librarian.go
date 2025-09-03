1e69464c05910d518d4eb586553d1c69de403ac4
Lawrence Qiu <lawrenceqiu@google.com>
2025-09-02 21:39:10 -0400

    chore: Clarify the code comments

--- internal/librarian/librarian.go
+++ internal/librarian/librarian.go
@@ -87,7 +87,9 @@ func lookupCommand(cmd *cli.Command, args []string) (*cli.Command, []string, err
 		cmd.Flags.Usage()
 		return nil, nil, err
 	}
-	// If the next argument matches a flag, parse the remaining args as flags
+	// If the next argument matches a potential flag (first char is `-`), parse the
+	// remaining arguments as flags. Check if argument is a flag before calling
+	// `lookupCommand` again to avoid flags from being treated as subcommands.
 	if len(args) > 1 && args[1][0] == '-' {
 		return subcommand, args[1:], nil
 	}
