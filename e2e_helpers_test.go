877937fb62176a9cbe23793df6b73a2513db2031
Jin Seop Kim <jinseop@google.com>
2025-09-05 15:59:46 -0400

    refactor(testing): move e2e test helpers to a shared file

---
+++ e2e_helpers_test.go
@@ -0,0 +1,48 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build e2e
+// +build e2e
+
+package librarian
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+)
+
+// initRepo initiates a git repo in the given directory, copy
+// files from source directory and create a commit.
+func initRepo(t *testing.T, dir, source string) error {
+	if err := os.CopyFS(dir, os.DirFS(source)); err != nil {
+		return err
+	}
+	runGit(t, dir, "init")
+	runGit(t, dir, "add", ".")
+	runGit(t, dir, "config", "user.email", "test@github.com")
+	runGit(t, dir, "config", "user.name", "Test User")
+	runGit(t, dir, "commit", "-m", "init test repo")
+	runGit(t, dir, "remote", "add", "origin", "https://github.com/googleapis/librarian.git")
+	return nil
+}
+
+func runGit(t *testing.T, dir string, args ...string) {
+	t.Helper()
+	cmd := exec.Command("git", args...)
+	cmd.Dir = dir
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("git %v: %v", args, err)
+	}
+}
--- generate_e2e_test.go
+++ generate_e2e_test.go
@@ -313,30 +313,6 @@ func TestRunConfigure(t *testing.T) {
 	}
 }
 
-// initRepo initiates a git repo in the given directory, copy
-// files from source directory and create a commit.
-func initRepo(t *testing.T, dir, source string) error {
-	if err := os.CopyFS(dir, os.DirFS(source)); err != nil {
-		return err
-	}
-	runGit(t, dir, "init")
-	runGit(t, dir, "add", ".")
-	runGit(t, dir, "config", "user.email", "test@github.com")
-	runGit(t, dir, "config", "user.name", "Test User")
-	runGit(t, dir, "commit", "-m", "init test repo")
-	runGit(t, dir, "remote", "add", "origin", "https://github.com/googleapis/librarian.git")
-	return nil
-}
-
 type genResponse struct {
 	ErrorMessage string `json:"error,omitempty"`
-}
-
-func runGit(t *testing.T, dir string, args ...string) {
-	t.Helper()
-	cmd := exec.Command("git", args...)
-	cmd.Dir = dir
-	if err := cmd.Run(); err != nil {
-		t.Fatalf("git %v: %v", args, err)
-	}
 }
