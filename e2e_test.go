e5f9701e25061c0b96f8f9207ab04294f88ba7bc
Lawrence Qiu <lawrenceqiu@google.com>
2025-09-08 14:49:17 -0400

    chore: Fix test issues

--- e2e_test.go
+++ e2e_test.go
@@ -244,9 +244,6 @@ func TestRunConfigure(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			workRoot := t.TempDir()
 			repo := t.TempDir()
-<<<<<<< HEAD
-			apiSourceRepo := t.TempDir()
-=======
 			for _, src := range []string{"example-source-path", "example-source-path-2"} {
 				srcPath := filepath.Join(repo, src)
 				if err := os.MkdirAll(srcPath, 0755); err != nil {
@@ -257,7 +254,6 @@ func TestRunConfigure(t *testing.T) {
 				}
 			}
 			APISourceRepo := t.TempDir()
->>>>>>> 9d173a2 (chore: Fix e2e tests)
 			if err := initRepo(t, repo, initialRepoStateDir); err != nil {
 				t.Fatalf("prepare test error = %v", err)
 			}
