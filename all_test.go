ed8b3d8954e4efe1eec394ebd79a367b623223c5
Joe Wang <joewa@google.com>
2025-09-11 15:51:33 -0400

    format

--- all_test.go
+++ all_test.go
@@ -203,7 +203,7 @@ func rungo(t *testing.T, args ...string) {
 func TestExportedSymbolsHaveDocs(t *testing.T) {
 	err := filepath.WalkDir(".", func(path string, d os.DirEntry, err error) error {
 		if err != nil || d.IsDir() || !strings.HasSuffix(path, ".go") ||
-				strings.HasSuffix(path, "_test.go") || strings.HasSuffix(path, ".pb.go") {
+			strings.HasSuffix(path, "_test.go") || strings.HasSuffix(path, ".pb.go") {
 			return nil
 		}
 
