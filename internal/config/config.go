1cefd244a71b28b43311adaeb5d724ce2ae9f4c4
Jeff Ching <chingor@google.com>
2025-09-16 13:03:04 -0700

    refactor: tag-and-release does not need local clone

--- internal/config/config.go
+++ internal/config/config.go
@@ -54,7 +54,8 @@ const (
 	// ReleaseInitResponse is a JSON file that describes which library to change
 	// after release.
 	ReleaseInitResponse = "release-init-response.json"
-	pipelineStateFile   = "state.yaml"
+	// PipelineStateFile is the name of the pipeline state file.
+	PipelineStateFile = "state.yaml"
 )
 
 // are variables so it can be replaced during testing.
@@ -294,7 +295,7 @@ func (c *Config) deriveRepo() error {
 	if err != nil {
 		return fmt.Errorf("getting working directory: %w", err)
 	}
-	stateFile := filepath.Join(wd, LibrarianDir, pipelineStateFile)
+	stateFile := filepath.Join(wd, LibrarianDir, PipelineStateFile)
 	if _, err := os.Stat(stateFile); err != nil {
 		return fmt.Errorf("repo flag not specified and no state file found in current working directory: %w", err)
 	}
