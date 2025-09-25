09de2ed85996463d6b712797af55310316e018d7
Carlos O'Ryan <coryan@google.com>
2025-09-25 19:03:06 -0400

    feat(sidekick): inline messages in discovery docs

--- internal/sidekick/internal/parser/discovery/message_fields.go
+++ internal/sidekick/internal/parser/discovery/message_fields.go
@@ -41,19 +41,24 @@ func makeField(model *api.API, message *api.Message, input *property) (*api.Fiel
 	if field, err := maybeMapField(model, message, input); err != nil || field != nil {
 		return field, err
 	}
-	if input.Schema.Type == "object" && input.Schema.Properties != nil {
-		// TODO(#2265) - handle inline object...
-		return nil, nil
+	if field, err := maybeInlineObjectField(model, message, input.Name, input.Schema); err != nil || field != nil {
+		return field, err
 	}
 	return makeScalarField(model, message, input.Name, input.Schema)
 }
 
 func makeArrayField(model *api.API, message *api.Message, input *property) (*api.Field, error) {
-	if input.Schema.ItemSchema.Type == "object" && input.Schema.ItemSchema.Properties != nil {
-		// TODO(#2265) - handle inline object...
-		return nil, nil
+	field, err := maybeInlineObjectField(model, message, input.Name, input.Schema.ItemSchema)
+	if err != nil {
+		return nil, err
 	}
-	field, err := makeScalarField(model, message, input.Name, input.Schema.ItemSchema)
+	if field != nil {
+		field.Documentation = input.Schema.Description
+		field.Repeated = true
+		field.Optional = false
+		return field, nil
+	}
+	field, err = makeScalarField(model, message, input.Name, input.Schema.ItemSchema)
 	if err != nil {
 		return nil, err
 	}
