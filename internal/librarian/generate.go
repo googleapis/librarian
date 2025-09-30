c19bb1a488c41f1e43dd7b0519951364e86aa245
Joe Wang <joewa@google.com>
2025-09-30 12:19:45 -0400

    copy library files

--- internal/librarian/generate.go
+++ internal/librarian/generate.go
@@ -407,7 +407,7 @@ func (r *generateRunner) runConfigureCommand(ctx context.Context, outputDir stri
 		r.state.Libraries[i] = libraryState
 	}
 
-	if err := cleanAndCopyLibrary(r.state, r.repo.GetDir(), libraryState.ID, outputDir); err != nil {
+	if err := copyLibraryFiles(r.state, r.repo.GetDir(), libraryState.ID, outputDir); err != nil {
 		return "", err
 	}
 
