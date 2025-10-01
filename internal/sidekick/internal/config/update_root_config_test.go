bd04edf6b9ebb2eccb1c363157b1844401b5922b
Carlos O'Ryan <coryan@google.com>
2025-10-01 10:07:48 -0400

    reactor(sidekick): more testable `UpdateRootConfig()`

--- internal/sidekick/internal/config/update_root_config_test.go
+++ internal/sidekick/internal/config/update_root_config_test.go
@@ -26,6 +26,12 @@ import (
 	toml "github.com/pelletier/go-toml/v2"
 )
 
+const (
+	testGitHubDn       = "https://localhost:12345"
+	testGitHubApi      = "https://localhost:23456"
+	tarballPathTrailer = "/archive/5d5b1bf126485b0e2c972bac41b376438601e266.tar.gz"
+)
+
 func TestUpdateRootConfig(t *testing.T) {
 	// update() normally writes `.sidekick.toml` to cwd. We need to change to a
 	// temporary directory to avoid changing the actual configuration, and any
@@ -101,5 +107,237 @@ func TestUpdateRootConfig(t *testing.T) {
 
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("mismatch in loaded root config (-want, +got)\n:%s", diff)
+	}
+}
+
+func TestGithubConfig(t *testing.T) {
+	got := githubConfig(&Config{})
+	want := &githubEndpoints{
+		Api:      defaultGitHubApi,
+		Download: defaultGitHubDn,
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("mismatch (-want, +got):\n%s", diff)
+	}
+
+	got = githubConfig(&Config{
+		Source: map[string]string{
+			"github-api": testGitHubApi,
+			"github":     testGitHubDn,
+		},
+	})
+	want = &githubEndpoints{
+		Api:      testGitHubApi,
+		Download: testGitHubDn,
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("mismatch (-want, +got):\n%s", diff)
+	}
+}
+
+func TestGithubRepoFromTarballLink(t *testing.T) {
+	config := &Config{
+		Source: map[string]string{
+			"github-api": testGitHubApi,
+			"github":     testGitHubDn,
+			"test-root":  testGitHubDn + "/org-name/repo-name" + tarballPathTrailer,
+		},
+	}
+	got, err := githubRepoFromTarballLink(config, "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := &githubRepo{
+		Org:  "org-name",
+		Repo: "repo-name",
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("mismatch (-want, +got):\n%s", diff)
+	}
+}
+
+func TestGithubRepoFromTarballLinkErrors(t *testing.T) {
+	for _, test := range []struct {
+		ExtraConfig map[string]string
+	}{
+		{ExtraConfig: map[string]string{}},
+		{ExtraConfig: map[string]string{"test-root": "too-short"}},
+	} {
+		config := &Config{
+			Source: map[string]string{
+				"github-api": testGitHubApi,
+				"github":     testGitHubDn,
+			},
+		}
+		for k, v := range test.ExtraConfig {
+			config.Source[k] = v
+		}
+		if got, err := githubRepoFromTarballLink(config, "test"); err == nil {
+			t.Errorf("expected an error, got=%v", got)
+		}
+	}
+}
+
+func TestUpdateRootConfigContents(t *testing.T) {
+	const (
+		inputContents = `# Test input for updateRootConfigContents
+test-extracted-name    = 'repo-name-old-commit'
+test-root              = 'https://github.com/org-name/repo-name/archive/old-commit.tar.gz'
+test-sha256            = 'old-sha256'
+untouched-extracted-name     = 'untouched-123'
+untouched-root               = 'https://github.com/org-name/repo-name/archive/refs/tags/123.gz'
+untouched-sha256             = 'untouched-sha256'
+`
+		output1 = `# Test input for updateRootConfigContents
+test-extracted-name    = 'repo-name-new-commit-1'
+test-root              = 'https://github.com/org-name/repo-name/archive/new-commit-1.tar.gz'
+test-sha256            = 'new-sha256-1'
+untouched-extracted-name     = 'untouched-123'
+untouched-root               = 'https://github.com/org-name/repo-name/archive/refs/tags/123.gz'
+untouched-sha256             = 'untouched-sha256'
+`
+		output2 = `# Test input for updateRootConfigContents
+test-extracted-name    = 'repo-name-new-commit-2'
+test-root              = 'https://github.com/org-name/repo-name/archive/new-commit-2.tar.gz'
+test-sha256            = 'new-sha256-2'
+untouched-extracted-name     = 'untouched-123'
+untouched-root               = 'https://github.com/org-name/repo-name/archive/refs/tags/123.gz'
+untouched-sha256             = 'untouched-sha256'
+`
+		inputContentsNoExtractedName = `# Test input for updateRootConfigContents
+test-root              = 'https://github.com/org-name/repo-name/archive/old-commit.tar.gz'
+test-sha256            = 'old-sha256'
+untouched-extracted-name     = 'untouched-123'
+untouched-root               = 'https://github.com/org-name/repo-name/archive/refs/tags/123.gz'
+untouched-sha256             = 'untouched-sha256'
+`
+		output3 = `# Test input for updateRootConfigContents
+test-root              = 'https://github.com/org-name/repo-name/archive/new-commit-3.tar.gz'
+test-sha256            = 'new-sha256-3'
+untouched-extracted-name     = 'untouched-123'
+untouched-root               = 'https://github.com/org-name/repo-name/archive/refs/tags/123.gz'
+untouched-sha256             = 'untouched-sha256'
+`
+		inputContentsNoNewline = `# Test input for updateRootConfigContents
+test-root              = 'https://github.com/org-name/repo-name/archive/old-commit.tar.gz'
+test-sha256            = 'old-sha256'
+# No newline at EOF`
+		output4 = `# Test input for updateRootConfigContents
+test-root              = 'https://github.com/org-name/repo-name/archive/new-commit-4.tar.gz'
+test-sha256            = 'new-sha256-4'
+# No newline at EOF`
+	)
+	for _, test := range []struct {
+		RootName  string
+		Input     string
+		LatestSha string
+		NewSha256 string
+		Want      string
+	}{
+		{
+			RootName:  "test",
+			Input:     inputContents,
+			LatestSha: "new-commit-1",
+			NewSha256: "new-sha256-1",
+			Want:      output1,
+		},
+		{
+			RootName:  "test",
+			Input:     inputContents,
+			LatestSha: "new-commit-2",
+			NewSha256: "new-sha256-2",
+			Want:      output2,
+		},
+		{
+			RootName:  "googleapis",
+			Input:     inputContents,
+			LatestSha: "new-commit-2",
+			NewSha256: "new-sha256-2",
+			Want:      inputContents,
+		},
+		{
+			RootName:  "test",
+			Input:     inputContentsNoExtractedName,
+			LatestSha: "new-commit-3",
+			NewSha256: "new-sha256-3",
+			Want:      output3,
+		},
+		{
+			RootName:  "test",
+			Input:     inputContentsNoNewline,
+			LatestSha: "new-commit-4",
+			NewSha256: "new-sha256-4",
+			Want:      output4,
+		},
+	} {
+		endpoints := &githubEndpoints{
+			Api:      defaultGitHubApi,
+			Download: defaultGitHubDn,
+		}
+		repo := &githubRepo{
+			Org:  "org-name",
+			Repo: "repo-name",
+		}
+		got, err := updateRootConfigContents(test.RootName, []byte(test.Input), endpoints, repo, test.LatestSha, test.NewSha256)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if diff := cmp.Diff(test.Want, string(got)); diff != "" {
+			t.Errorf("mismatch (-want, +got):\n%s", diff)
+		}
+	}
+}
+
+func TestUpdateRootConfigContentsErrors(t *testing.T) {
+	const (
+		badRoot = `# Test input for updateRootConfigContents
+test-extracted-name    = 'repo-name-old-commit'
+test-root # Missing separator
+test-sha256            = 'old-sha256'
+`
+		badSha256 = `# Test input for updateRootConfigContents
+test-extracted-name    = 'repo-name-old-commit'
+test-root              = 'https://github.com/org-name/repo-name/archive/old-commit.tar.gz'
+test-sha256 # Missing separator
+`
+
+		badExtractedName = `# Test input for updateRootConfigContents
+test-extracted-name # Missing separator
+test-root              = 'https://github.com/org-name/repo-name/archive/old-commit.tar.gz'
+test-sha256            = 'old-sha256'
+`
+
+		tooManyRoots = `# Test input for updateRootConfigContents
+test-extracted-name    = 'repo-name-old-commit'
+test-root              = 'https://github.com/org-name/repo-name/archive/old-commit.tar.gz'
+test-root              = 'https://github.com/org-name/repo-name/archive/old-commit.tar.gz'
+test-sha256            = 'old-sha256'
+`
+		tooManySha256 = `# Test input for updateRootConfigContents
+test-extracted-name    = 'repo-name-old-commit'
+test-root              = 'https://github.com/org-name/repo-name/archive/old-commit.tar.gz'
+test-sha256            = 'old-sha256'
+test-sha256            = 'old-sha256'
+`
+		tooManyExtractedNames = `# Test input for updateRootConfigContents
+test-extracted-name    = 'repo-name-old-commit'
+test-extracted-name    = 'repo-name-old-commit'
+test-root              = 'https://github.com/org-name/repo-name/archive/old-commit.tar.gz'
+test-sha256            = 'old-sha256'
+`
+	)
+	for idx, test := range []string{badRoot, badSha256, badExtractedName, tooManyRoots, tooManySha256, tooManyExtractedNames} {
+		endpoints := &githubEndpoints{
+			Api:      defaultGitHubApi,
+			Download: defaultGitHubDn,
+		}
+		repo := &githubRepo{
+			Org:  "org-name",
+			Repo: "repo-name",
+		}
+		if got, err := updateRootConfigContents("test", []byte(test), endpoints, repo, "unused", "unused"); err == nil {
+			t.Errorf("expected an error in updateRootConfigContents[%d], got=%q", idx, got)
+		}
 	}
 }
