be6eb312fbb9bbba93c02bbc647216a2ed345a00
Jeff Ching <chingor@google.com>
2025-10-10 15:45:26 -0700

    feat: add ability to find the latest docker image SHA

---
+++ internal/images/images.go
@@ -0,0 +1,160 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package images provides operations around docker images.
+package images
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"strings"
+
+	artifactregistry "cloud.google.com/go/artifactregistry/apiv1"
+	artifactregistrypb "cloud.google.com/go/artifactregistry/apiv1/artifactregistrypb"
+)
+
+// ImageRegistryClient is an abstration of a Docker registry client.
+type ImageRegistryClient interface {
+	// FindLatest returns the latest docker image given a current image.
+	FindLatest(ctx context.Context, image *Image) (string, error)
+}
+
+// ArtifactRegistryClient is the implementation of ImageRegistryClient
+// to interact with Artifact Registry.
+type ArtifactRegistryClient struct {
+}
+
+// Image is a data structure for parsing Docker image parameters.
+type Image struct {
+	Name       string
+	Tag        string
+	SHA        string
+	Location   string
+	Project    string
+	Repository string
+}
+
+// BaseName returns the image name without a pinned SHA or tag.
+func (i *Image) BaseName() string {
+	return fmt.Sprintf("%s-docker.pkg.dev/%s/%s/%s", i.Location, i.Project, i.Repository, i.Name)
+}
+
+// String returns the image name with pinned SHA or tag.
+func (i *Image) String() string {
+	var b strings.Builder
+	b.WriteString(i.BaseName())
+	if i.SHA != "" {
+		b.WriteString("@")
+		b.WriteString(i.SHA)
+	} else if i.Tag != "" {
+		b.WriteString(":")
+		b.WriteString(i.Tag)
+	}
+	return b.String()
+}
+
+// FindLatest returns the latest docker image given a current image.
+func (c *ArtifactRegistryClient) FindLatest(ctx context.Context, image *Image) (string, error) {
+	client, err := artifactregistry.NewClient(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer client.Close()
+
+	it := client.ListVersions(ctx, &artifactregistrypb.ListVersionsRequest{
+		Parent:  fmt.Sprintf("projects/%s/locations/%s/repositories/%s/packages/%s", image.Project, image.Location, image.Repository, image.Name),
+		View:    artifactregistrypb.VersionView_FULL,
+		OrderBy: "create_time DESC",
+	})
+	version, err := it.Next()
+	if err != nil {
+		return "", err
+	}
+	slog.Info("Found packages version", "version", version.GetName())
+
+	// latest SHA is found as the "subjectDigest" metadata field
+	latestSha := ""
+	for key, field := range version.GetMetadata().GetFields() {
+		if key == "subjectDigest" {
+			slog.Info("Found SHA", "sha", field.GetStringValue())
+			latestSha = field.GetStringValue()
+		}
+	}
+
+	if latestSha == "" {
+		return "", fmt.Errorf("failed to find updated SHA for latest version: %s", version.GetName())
+	}
+
+	newImage := &Image{
+		Name:       image.Name,
+		Location:   image.Location,
+		Repository: image.Repository,
+		Project:    image.Project,
+		SHA:        latestSha,
+	}
+	return newImage.String(), nil
+}
+
+// FindLatestImage returns the latest created image matching the provided image name.
+func FindLatestImage(ctx context.Context, client ImageRegistryClient, currentImage string) (string, error) {
+	image, err := parseImage(currentImage)
+	if err != nil {
+		return "", err
+	}
+
+	latestImage, err := client.FindLatest(ctx, image)
+	if err != nil {
+		return "", err
+	}
+	return latestImage, nil
+}
+
+func parseImage(pinnedImage string) (*Image, error) {
+	parsedImage := &Image{}
+	baseName := ""
+
+	atParts := strings.Split(pinnedImage, "@")
+	colonParts := strings.Split(pinnedImage, ":")
+	if len(atParts) == 2 {
+		baseName = atParts[0]
+		parsedImage.SHA = atParts[1]
+	} else if len(colonParts) == 2 {
+		baseName = colonParts[0]
+		parsedImage.Tag = colonParts[1]
+	}
+
+	if baseName == "" {
+		slog.Info("image does not appear to be pinned")
+		baseName = pinnedImage
+	}
+
+	parts := strings.Split(baseName, "/")
+	if len(parts) < 4 {
+		return nil, fmt.Errorf("unexpected image format, expected an AR formatted image: %s", baseName)
+	}
+
+	host := parts[0]
+	if strings.HasSuffix(host, "-docker.pkg.dev") {
+		parsedImage.Location = strings.Replace(host, "-docker.pkg.dev", "", 1)
+	} else {
+		return nil, fmt.Errorf("expected AR formatted host with -docker.pkg.dev suffix: %s", host)
+	}
+
+	parsedImage.Project = parts[1]
+	parsedImage.Repository = parts[2]
+	parsedImage.Name = parts[3]
+
+	return parsedImage, nil
+}
---
+++ internal/images/images_test.go
@@ -0,0 +1,145 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package images provides operations around docker images.
+package images
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+type MockImageRegistryClient struct {
+	LatestImage string
+	Error       error
+}
+
+func (c *MockImageRegistryClient) FindLatest(ctx context.Context, image *Image) (string, error) {
+	return c.LatestImage, c.Error
+}
+
+func TestFindLatestImage(t *testing.T) {
+	for _, test := range []struct {
+		name    string
+		image   string
+		want    string
+		client  ImageRegistryClient
+		wantErr bool
+	}{
+		{
+			name:  "AR unpinned",
+			image: "us-central1-docker.pkg.dev/some-project/some-repo/some-image",
+			want:  "us-central1-docker.pkg.dev/some-project/some-repo/some-image@sha256:abcdef1234",
+			client: &MockImageRegistryClient{
+				LatestImage: "us-central1-docker.pkg.dev/some-project/some-repo/some-image@sha256:abcdef1234",
+			},
+		},
+		{
+			name:  "AR pinned",
+			image: "us-central1-docker.pkg.dev/some-project/some-repo/some-image@sha256:123abc",
+			want:  "us-central1-docker.pkg.dev/some-project/some-repo/some-image@sha256:abcdef1234",
+			client: &MockImageRegistryClient{
+				LatestImage: "us-central1-docker.pkg.dev/some-project/some-repo/some-image@sha256:abcdef1234",
+			},
+		},
+		{
+			name:    "AR pinned",
+			image:   "us-central1-docker.pkg.dev/some-project/some-repo/some-image@sha256:123abc",
+			wantErr: true,
+			client: &MockImageRegistryClient{
+				Error: fmt.Errorf("test error"),
+			},
+		},
+		{
+			name:    "invalid image",
+			image:   "gcr.io/some-project/some-name",
+			wantErr: true,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := FindLatestImage(t.Context(), test.client, test.image)
+			if test.wantErr {
+				if err == nil {
+					t.Errorf("FindLatestImage() error = %v, wantErr %v", err, test.wantErr)
+				}
+				return
+			}
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("FindLatestImage() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestParseImage(t *testing.T) {
+	for _, test := range []struct {
+		name    string
+		image   string
+		want    *Image
+		wantErr bool
+	}{
+		{
+			name:  "AR unpinned",
+			image: "us-central1-docker.pkg.dev/some-project/some-repo/some-image",
+			want: &Image{
+				Name:       "some-image",
+				Location:   "us-central1",
+				Project:    "some-project",
+				Repository: "some-repo",
+			},
+		},
+		{
+			name:  "AR pinned SHA",
+			image: "us-central1-docker.pkg.dev/some-project/some-repo/some-image@sha256:abcdef",
+			want: &Image{
+				Name:       "some-image",
+				Location:   "us-central1",
+				Project:    "some-project",
+				Repository: "some-repo",
+				SHA:        "sha256:abcdef",
+			},
+		},
+		{
+			name:  "AR tagged",
+			image: "us-central1-docker.pkg.dev/some-project/some-repo/some-image:1.2.3",
+			want: &Image{
+				Name:       "some-image",
+				Location:   "us-central1",
+				Project:    "some-project",
+				Repository: "some-repo",
+				Tag:        "1.2.3",
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := parseImage(test.image)
+			if (err != nil) != test.wantErr {
+				t.Errorf("parseImage() error = %v, wantErr %v", err, test.wantErr)
+				return
+			}
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("parseImage() mismatch (-want +got):\n%s", diff)
+			}
+			str := got.String()
+			if diff := cmp.Diff(str, test.image); diff != "" {
+				t.Errorf("image.String() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
