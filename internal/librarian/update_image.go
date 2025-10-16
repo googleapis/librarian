4f366608770774ea867aa33e3454f8dbf6babdaf
Jeff Ching <chingor@google.com>
2025-10-16 16:45:04 -0700

    feat: open update-image PR as draft if there are generation failures

--- internal/librarian/update_image.go
+++ internal/librarian/update_image.go
@@ -140,7 +140,6 @@ func (r *updateImageRunner) run(ctx context.Context) error {
 		return body, nil
 	}
 	commitMessage := fmt.Sprintf("feat: update image to %s", r.image)
-	// TODO(#2588): open PR as draft if there are failures
 	return commitAndPush(ctx, &commitInfo{
 		branch:            r.branch,
 		commit:            r.commit,
@@ -155,6 +154,7 @@ func (r *updateImageRunner) run(ctx context.Context) error {
 		workRoot:          r.workRoot,
 		failedGenerations: len(failedGenerations),
 		prBodyBuilder:     prBodyBuilder,
+		isDraft:           len(failedGenerations) > 0,
 	})
 }
 
