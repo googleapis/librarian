9d8945e148319dcf784e49017b5aac6db8141318
Joe Wang <joewa@google.com>
2025-10-17 14:55:45 -0400

    chore: remove redundant function

--- internal/librarian/generate_pull_request.go
+++ internal/librarian/generate_pull_request.go
@@ -164,7 +164,7 @@ func formatOnboardPRBody(request *onboardPRRequest) (string, error) {
 
 // getPiperID extracts the Piper ID from the commit message that onboarded the API.
 func getPiperID(state *config.LibrarianState, sourceRepo gitrepo.Repository, apiPath, library string) (string, error) {
-	libraryState := findLibraryByID(state, library)
+	libraryState := state.LibraryByID(library)
 	serviceYaml := ""
 	for _, api := range libraryState.APIs {
 		if api.Path == apiPath {
