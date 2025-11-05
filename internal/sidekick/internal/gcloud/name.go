1c95266c285623bfdd93f5fd10149905bc4d405f
Santiago Quiroga <santiquiroga@google.com>
2025-11-05 15:00:28 -0800

    chore(surfer): add method parsing package

---
+++ internal/sidekick/internal/gcloud/name.go
@@ -0,0 +1,36 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcloud
+
+import (
+	"regexp"
+	"strings"
+)
+
+var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+var matchAllCap   = regexp.MustCompile("([a-z0-9])([A-Z])")
+
+// toSnakeCase converts a string to snake_case.
+func toSnakeCase(str string) string {
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
+}
+
+// ToKebabCase converts a string to kebab-case.
+func ToKebabCase(str string) string {
+	snake := toSnakeCase(str)
+	return strings.ReplaceAll(snake, "_", "-")
+}
