0ab77cf9aa9d3de97aa53c28c1a2dad244bcd47e
Joe Wang <joewa@google.com>
2025-11-06 17:04:08 -0500

    add automation runner

--- internal/automation/automation.go
+++ internal/automation/automation.go
@@ -1,1 +1,38 @@
 package automation
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/googleapis/librarian/internal/config"
+)
+
+// runCommandFn is a function type that matches RunCommand, for mocking in tests.
+var runCommandFn = RunCommand
+
+type automationRunner struct {
+	build    bool
+	command  string
+	forceRun bool
+	project  string
+	push     bool
+}
+
+func newAutomationRunner(cfg *config.AutomationConfig) *automationRunner {
+	return &automationRunner{
+		build:    cfg.Build,
+		command:  cfg.Command,
+		forceRun: cfg.ForceRun,
+		project:  cfg.Project,
+		push:     cfg.Push,
+	}
+}
+
+func (r *automationRunner) run(ctx context.Context) error {
+	err := runCommandFn(ctx, r.command, r.project, r.push, r.build, r.forceRun)
+	if err != nil {
+		slog.Error("error running command", slog.Any("err", err))
+		return err
+	}
+	return nil
+}
