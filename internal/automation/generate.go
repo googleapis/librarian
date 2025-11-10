f1ef7a58d29c020025348ff776abc5d7d804445a
Joe Wang <joewa@google.com>
2025-11-10 16:42:15 -0500

    feat: refactor generate commond in automation package

--- internal/automation/generate.go
+++ internal/automation/generate.go
@@ -21,19 +21,23 @@ import (
 )
 
 const (
-	publishCmdName = "publish-release"
+	generateCmdName = "generate"
 )
 
-type publishRunner struct {
+type generateRunner struct {
+	build     bool
 	projectID string
+	push      bool
 }
 
-func newPublishRunner(cfg *config.Config) *publishRunner {
-	return &publishRunner{
+func newGenerateRunner(cfg *config.Config) *generateRunner {
+	return &generateRunner{
+		build:     cfg.Build,
 		projectID: cfg.Project,
+		push:      cfg.Push,
 	}
 }
 
-func (r *publishRunner) run(ctx context.Context) error {
-	return runCommandFn(ctx, publishCmdName, r.projectID, false, false)
+func (r *generateRunner) run(ctx context.Context) error {
+	return runCommandFn(ctx, generateCmdName, r.projectID, false, r.build)
 }
