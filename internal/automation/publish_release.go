ddc837865ed87e52fd77984038064281e808b09a
Joe Wang <joewa@google.com>
2025-11-10 10:00:01 -0500

    use RunCommand

--- internal/automation/publish_release.go
+++ internal/automation/publish_release.go
@@ -16,16 +16,8 @@ package automation
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"log/slog"
-	"os"
-	"strings"
 
-	cloudbuild "cloud.google.com/go/cloudbuild/apiv1/v2"
 	"github.com/googleapis/librarian/internal/config"
-	"github.com/googleapis/librarian/internal/github"
-	"google.golang.org/api/option"
 )
 
 const (
@@ -33,78 +25,19 @@ const (
 )
 
 type publishRunner struct {
-	cloudBuildClient CloudBuildClient
-	ghClient         GitHubClient
-	repoConfig       *RepositoriesConfig
-	forceRun         bool
-	projectID        string
-	push             bool
+	forceRun  bool
+	projectID string
+	push      bool
 }
 
-var clientFactory = func(ctx context.Context, opts ...option.ClientOption) (*cloudbuild.Client, error) {
-	return cloudbuild.NewClient(ctx, opts...)
-}
-
-func newPublishRunner(ctx context.Context, cfg *config.Config) (*publishRunner, error) {
-	client, err := clientFactory(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("error creating cloudbuild client: %w", err)
-	}
-	wrappedClient := &wrappedCloudBuildClient{
-		client: client,
-	}
-	ghClient := github.NewClient(os.Getenv(config.LibrarianGithubToken), nil)
-	repoConfig, err := loadRepositoriesConfig()
-	if err != nil {
-		return nil, fmt.Errorf("error loading repositories config: %w", err)
-	}
+func newPublishRunner(cfg *config.Config) *publishRunner {
 	return &publishRunner{
-		cloudBuildClient: wrappedClient,
-		ghClient:         ghClient,
-		repoConfig:       repoConfig,
-		forceRun:         cfg.ForceRun,
-		projectID:        cfg.Project,
-		push:             cfg.Push,
-	}, nil
+		forceRun:  cfg.ForceRun,
+		projectID: cfg.Project,
+		push:      cfg.Push,
+	}
 }
 
 func (r *publishRunner) run(ctx context.Context) error {
-	errs := make([]error, 0)
-	repositories := r.repoConfig.RepositoriesForCommand(publishCmdName)
-	for _, repository := range repositories {
-		slog.Debug("running command", "command", publishCmdName, "repository", repository.Name)
-		gitUrl, err := repository.GitURL()
-		if err != nil {
-			slog.Error("repository has no configured git url", slog.Any("repository", repository))
-			return err
-		}
-
-		substitutions := map[string]string{
-			"_REPOSITORY":               repository.Name,
-			"_FULL_REPOSITORY":          gitUrl,
-			"_GITHUB_TOKEN_SECRET_NAME": repository.SecretName,
-			"_PUSH":                     fmt.Sprintf("%v", r.push),
-		}
-		parts := strings.Split(gitUrl, "/")
-		repositoryOwner := parts[len(parts)-2]
-		prs, err := r.ghClient.FindMergedPullRequestsWithPendingReleaseLabel(ctx, repositoryOwner, repository.Name)
-		if err != nil {
-			slog.Error("error finding merged pull requests for publish-release", slog.Any("err", err), slog.String("repository", repository.Name))
-			errs = append(errs, err)
-			continue
-		}
-		if len(prs) == 0 {
-			slog.Info("no pull requests with label 'release:pending' found. Skipping 'publish-release' trigger.", slog.String("repository", repository.Name))
-			continue
-		} else {
-			substitutions["_PR"] = fmt.Sprintf("%v", prs[0].GetHTMLURL())
-		}
-		err = runCloudBuildTriggerByName(ctx, r.cloudBuildClient, r.projectID, region, publishCmdName, substitutions)
-		if err != nil {
-			slog.Error("error triggering cloudbuild", slog.Any("err", err))
-			errs = append(errs, err)
-		}
-	}
-
-	return errors.Join(errs...)
+	return RunCommand(ctx, publishCmdName, r.projectID, r.push, false, r.forceRun)
 }
--- internal/automation/publish_release_test.go
+++ internal/automation/publish_release_test.go
@@ -15,28 +15,16 @@
 package automation
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"strings"
 	"testing"
 
-	cloudbuild "cloud.google.com/go/cloudbuild/apiv1/v2"
-	"cloud.google.com/go/cloudbuild/apiv1/v2/cloudbuildpb"
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v69/github"
 	"github.com/googleapis/librarian/internal/config"
-	"google.golang.org/api/option"
 )
 
 func TestNewPublishRunner(t *testing.T) {
 	t.Parallel()
 	for _, test := range []struct {
-		name          string
-		cfg           *config.Config
-		clientFactory func(ctx context.Context, opts ...option.ClientOption) (*cloudbuild.Client, error)
-		wantErr       bool
-		wantErrMsg    string
+		name string
+		cfg  *config.Config
 	}{
 		{
 			name: "create_a_runner",
@@ -45,53 +33,11 @@ func TestNewPublishRunner(t *testing.T) {
 				Project:  "example-project",
 				Push:     true,
 			},
-			clientFactory: func(ctx context.Context, opts ...option.ClientOption) (*cloudbuild.Client, error) {
-				// Force WithoutAuthentication for these tests
-				return cloudbuild.NewClient(ctx, option.WithoutAuthentication())
-			},
-		},
-		{
-			name: "create_cloud_build_client_failed",
-			cfg: &config.Config{
-				ForceRun: true,
-				Project:  "example-project",
-				Push:     true,
-			},
-			clientFactory: func(ctx context.Context, opts ...option.ClientOption) (*cloudbuild.Client, error) {
-				// Force WithoutAuthentication for these tests
-				return nil, errors.New("simulated error")
-			},
-			wantErr:    true,
-			wantErrMsg: "error creating cloudbuild client",
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
-			// Do not run in parallel because tests are changing the global package-level variable clientFactory.
-			ctx := t.Context()
-			clientFactory = test.clientFactory
-			runner, err := newPublishRunner(ctx, test.cfg)
-			if test.wantErr {
-				if err == nil {
-					t.Errorf("newPublishRunner() want error but got nil")
-					return
-				}
-
-				if !strings.Contains(err.Error(), test.wantErrMsg) {
-					t.Errorf("want error message: %q, got %q", test.wantErrMsg, err.Error())
-				}
-				return
-			}
-			if err != nil {
-				t.Errorf("newPublishRunner() got error: %v", err)
-				return
-			}
-
-			if runner.cloudBuildClient == nil {
-				t.Errorf("newPublishRunner() cloudBuildClient is not set")
-			}
-			if runner.ghClient == nil {
-				t.Errorf("newPublishRunner() ghClient is not set")
-			}
+			t.Parallel()
+			runner := newPublishRunner(test.cfg)
 			if runner.forceRun != test.cfg.ForceRun {
 				t.Errorf("newPublishRunner() forceRun is not set")
 			}
@@ -100,120 +46,6 @@ func TestNewPublishRunner(t *testing.T) {
 			}
 			if runner.push != test.cfg.Push {
 				t.Errorf("newPublishRunner() push is not set")
-			}
-		})
-	}
-}
-
-func TestPublishRunnerRun(t *testing.T) {
-	t.Parallel()
-	for _, test := range []struct {
-		name               string
-		push               bool
-		forceRun           bool
-		repositoriesConfig *RepositoriesConfig
-		want               string
-		runError           error
-		wantErr            bool
-		buildTriggers      []*cloudbuildpb.BuildTrigger
-		ghPRs              []*github.PullRequest
-		ghError            error
-		wantTriggersRun    []string
-	}{
-		{
-			name:     "runs publish-release trigger",
-			push:     true,
-			forceRun: true,
-			repositoriesConfig: &RepositoriesConfig{
-				Repositories: []*RepositoryConfig{
-					{
-						SupportedCommands: []string{"publish-release"},
-						Name:              "librarian",
-					},
-				},
-			},
-			buildTriggers: []*cloudbuildpb.BuildTrigger{
-				{
-					Name: "publish-release",
-					Id:   "publish-release-trigger-id",
-				},
-			},
-			ghPRs:           []*github.PullRequest{{HTMLURL: github.Ptr("https://github.com/googleapis/librarian/pull/1")}},
-			wantTriggersRun: []string{"publish-release-trigger-id"},
-		},
-		{
-			name:     "skips publish-release with no PRs",
-			push:     true,
-			forceRun: true,
-			repositoriesConfig: &RepositoriesConfig{
-				Repositories: []*RepositoryConfig{
-					{
-						SupportedCommands: []string{"publish-release"},
-						Name:              "librarian",
-					},
-				},
-			},
-			ghPRs:           []*github.PullRequest{},
-			wantTriggersRun: nil,
-		},
-		{
-			name:     "error finding PRs for publish-release",
-			push:     true,
-			forceRun: true,
-			repositoriesConfig: &RepositoriesConfig{
-				Repositories: []*RepositoryConfig{
-					{
-						SupportedCommands: []string{"publish-release"},
-						Name:              "librarian",
-					},
-				},
-			},
-			wantErr:         true,
-			ghError:         fmt.Errorf("github error"),
-			wantTriggersRun: nil,
-		},
-		{
-			name:     "error finding repository",
-			push:     true,
-			forceRun: true,
-			repositoriesConfig: &RepositoriesConfig{
-				Repositories: []*RepositoryConfig{
-					{
-						SupportedCommands: []string{"publish-release"},
-					},
-				},
-			},
-			wantErr:         true,
-			wantTriggersRun: nil,
-		},
-	} {
-		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
-			ctx := t.Context()
-			cloudBuildClient := &mockCloudBuildClient{
-				runError:      test.runError,
-				buildTriggers: test.buildTriggers,
-			}
-			ghClient := &mockGitHubClient{
-				prs: test.ghPRs,
-				err: test.ghError,
-			}
-			runner := &publishRunner{
-				cloudBuildClient: cloudBuildClient,
-				ghClient:         ghClient,
-				forceRun:         test.forceRun,
-				repoConfig:       test.repositoriesConfig,
-				projectID:        "some-project",
-				push:             test.push,
-			}
-			err := runner.run(ctx)
-			if test.wantErr && err == nil {
-				t.Fatal("expected error, but did not return one")
-			} else if !test.wantErr && err != nil {
-				t.Errorf("did not expect error, but received one: %s", err)
-			}
-			if diff := cmp.Diff(test.wantTriggersRun, cloudBuildClient.triggersRun); diff != "" {
-				t.Errorf("Run() triggersRun diff (-want, +got):\n%s", diff)
 			}
 		})
 	}
