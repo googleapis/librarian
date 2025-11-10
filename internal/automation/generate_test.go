564f7682798dbf472bc3be5ff967c957c9dd1029
Joe Wang <joewa@google.com>
2025-11-10 16:47:05 -0500

    add unit tests

---
+++ internal/automation/generate_test.go
@@ -0,0 +1,82 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package automation
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/googleapis/librarian/internal/config"
+)
+
+func TestNewGenerateRunner(t *testing.T) {
+	t.Parallel()
+	for _, test := range []struct {
+		name string
+		cfg  *config.Config
+	}{
+		{
+			name: "create_a_runner",
+			cfg: &config.Config{
+				Build:   true,
+				Project: "example-project",
+				Push:    true,
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			runner := newGenerateRunner(test.cfg)
+			if runner.build != test.cfg.Build {
+				t.Errorf("newGenerateRunner() build is not set")
+			}
+			if runner.projectID != test.cfg.Project {
+				t.Errorf("newGenerateRunner() projectID is not set")
+			}
+			if runner.push != test.cfg.Push {
+				t.Errorf("newGenerateRunner() push is not set")
+			}
+		})
+	}
+}
+
+func TestGenerateRunnerRun(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name          string
+		args          []string
+		runCommandErr error
+		wantErr       bool
+	}{
+		{
+			name:          "error from RunCommand",
+			runCommandErr: errors.New("run command failed"),
+			wantErr:       true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			runCommandFn = func(ctx context.Context, command string, projectId string, push bool, build bool) error {
+				return test.runCommandErr
+			}
+			runner := &generateRunner{}
+			if err := runner.run(t.Context()); (err != nil) != test.wantErr {
+				t.Errorf("run() error = %v, wantErr %v", err, test.wantErr)
+			}
+		})
+	}
+}
