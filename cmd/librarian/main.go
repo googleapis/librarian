86383334df39553e6d808ac4f037b09dc28a2d7f
Julie Qiu <julie@golang.org>
2025-11-22 23:47:06 -0500

    update package doc

--- cmd/librarian/main.go
+++ cmd/librarian/main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Command librarian creates, generates, and release client libraries.
+// Command librarian creates, generates, and releases client libraries.
 package main
 
 import (
