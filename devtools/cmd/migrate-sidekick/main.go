e67368c2b4d8f69ace6550d2ceeb2fa66b5348a2
Joe Wang <joewa@google.com>
2025-12-02 15:37:09 -0500

    change package

--- devtools/cmd/migrate-sidekick/main.go
+++ devtools/cmd/migrate-sidekick/main.go
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package migrate_sidekick provides tools to create a librarian configuration from .sidekick.toml
-// in a repository.
-package migrate_sidekick
+package main
 
 import (
 	"errors"
