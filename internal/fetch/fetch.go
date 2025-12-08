a16a9c72ae222b79d659304fe30eae5088bbf8d5
Joe Wang <joewa@google.com>
2025-12-08 10:35:32 -0500

    fix: get sha256 from a given commit

--- internal/fetch/fetch.go
+++ internal/fetch/fetch.go
@@ -125,12 +125,21 @@ func LatestCommitAndChecksum(endpoints *Endpoints, repo *Repo) (commit, sha256 s
 		return "", "", err
 	}
 
-	tarballURL := TarballLink(endpoints.Download, repo, commit)
-	sha256, err = urlSha256(tarballURL)
+	sha256, err = Checksum(endpoints, repo, commit)
 	if err != nil {
 		return "", "", err
 	}
 	return commit, sha256, nil
+}
+
+// Checksum returns the SHA256 for the given repository at the given commit.
+func Checksum(endpoints *Endpoints, repo *Repo, commit string) (sha256 string, err error) {
+	tarballURL := TarballLink(endpoints.Download, repo, commit)
+	sha256, err = urlSha256(tarballURL)
+	if err != nil {
+		return "", err
+	}
+	return sha256, nil
 }
 
 // TarballLink constructs a GitHub tarball download URL for the given
