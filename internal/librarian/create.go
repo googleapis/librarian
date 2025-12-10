bb1d2cefd268e5a32de1d27aa5b26d5c93faf6e7
Laura Detmer <ldetmer@google.com>
2025-12-10 16:06:15 -0500

    feat: add create command to librarian

---
+++ internal/librarian/create.go
@@ -0,0 +1,113 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package librarian
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+
+	"github.com/googleapis/librarian/internal/config"
+	"github.com/googleapis/librarian/internal/librarian/internal/rust"
+	"github.com/googleapis/librarian/internal/yaml"
+	"github.com/urfave/cli/v3"
+)
+
+var (
+	errRunningCreateOnNonRustLanguage = errors.New("library creation is only supported for Rust libraries")
+	errOutputFlagRequired             = errors.New("output flag is required when default.output is not set in librarian.yaml")
+	errServiceConfigAndSpecRequired   = errors.New("both service-config and specification-source flags are required for creating a new library")
+	errMissingNameFlag                = errors.New("name flag is required to create a new library")
+)
+
+func createCommand() *cli.Command {
+	return &cli.Command{
+		Name:      "create",
+		Usage:     "create a new client library",
+		UsageText: "librarian create --name <name> --specification-source <path> --service-config <path>",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "name",
+				Usage: "library name",
+			},
+			&cli.StringFlag{
+				Name:  "specification-source",
+				Usage: "path to the specification source (e.g., google/cloud/secretmanager/v1)",
+			},
+			&cli.StringFlag{
+				Name:  "service-config",
+				Usage: "path to the service config",
+			},
+			&cli.StringFlag{
+				Name:  "output",
+				Usage: "output directory (optional, will be derived if not provided)",
+			},
+			&cli.StringFlag{
+				Name:  "specification-format",
+				Usage: "specification format (e.g., protobuf, discovery)",
+				Value: "protobuf",
+			},
+		},
+		Action: func(ctx context.Context, c *cli.Command) error {
+			name := c.String("name")
+			specSource := c.String("specification-source")
+			serviceConfig := c.String("service-config")
+			output := c.String("output")
+			specFormat := c.String("specification-format")
+			if name == "" {
+				return errMissingNameFlag
+			}
+			return runCreate(ctx, name, specSource, serviceConfig, output, specFormat)
+		},
+	}
+}
+
+func runCreate(ctx context.Context, name, specSource, serviceConfig, output, specFormat string) error {
+	cfg, err := yaml.Read[config.Config](librarianConfigPath)
+	if err != nil {
+		return err
+	}
+
+	if cfg.Language != "rust" {
+		return errRunningCreateOnNonRustLanguage
+	}
+
+	if output == "" {
+		if cfg.Default == nil || cfg.Default.Output == "" {
+			return errOutputFlagRequired
+		}
+		output = rust.DefaultOutput(specSource, cfg.Default.Output)
+	}
+	libraryExists := false
+	for _, lib := range cfg.Libraries {
+		if lib.Name == name {
+
+			libraryExists = true
+			break
+		}
+	}
+
+	if libraryExists {
+		return runGenerate(ctx, false, name)
+	}
+
+	if (serviceConfig == "") || (specSource == "") {
+		return errServiceConfigAndSpecRequired
+	}
+
+	// TODO: port over sidekick rustGenerate logic to create a new librarian
+	slog.Info("Creating new Rust library (not implemented yet)")
+	return nil
+}
---
+++ internal/librarian/create_test.go
@@ -0,0 +1,96 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package librarian
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestCreateCommand(t *testing.T) {
+	const (
+		libExists       = "library-one"
+		libExistsOutput = "output1"
+		newLib          = "library-two"
+		newLibOutput    = "output2"
+		newLibSpec      = "google/cloud/storage/v1"
+		newLibSC        = "google/cloud/storage/v1/storage_v1.yaml"
+	)
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	googleapisDir := filepath.Join(wd, "testdata", "googleapis")
+
+	tempDir := t.TempDir()
+	t.Chdir(tempDir)
+	configPath := filepath.Join(tempDir, librarianConfigPath)
+
+	configContent := fmt.Sprintf(`language: rust
+sources:
+  googleapis:
+    dir: %s
+libraries:
+  - name: %s
+    output: %s
+    apis:
+      - path: google/cloud/speech/v1
+      - path: google/cloud/speech/v1p1beta1
+      - path: google/cloud/speech/v2
+      - path: grafeas/v1
+`, googleapisDir, libExists, libExistsOutput)
+	if err := os.WriteFile(configPath, []byte(configContent), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create the output directories that are referenced in the config
+	if err := os.MkdirAll(filepath.Join(tempDir, libExistsOutput), 0755); err != nil {
+		t.Fatal(err)
+	}
+
+	for _, test := range []struct {
+		name    string
+		args    []string
+		wantErr error
+		want    []string
+	}{
+		{
+			name:    "no args",
+			args:    []string{"librarian", "create"},
+			wantErr: errMissingNameFlag,
+		},
+		{
+			name: "run create for existing library",
+			args: []string{"librarian", "create", "--name", libExists, "--output", libExistsOutput},
+			want: []string{},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			err := Run(t.Context(), test.args...)
+			if test.wantErr != nil {
+				if !errors.Is(err, test.wantErr) {
+					t.Errorf("want error %v, got %v", test.wantErr, err)
+				}
+				return
+			}
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
