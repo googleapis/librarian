28e85ecdb779344e3063bb9f5679390839b417a3
Laura Detmer <ldetmer@google.com>
2025-12-15 13:35:56 -0500

    fixed test data

--- internal/librarian/rust/helper_test.go
+++ internal/librarian/rust/helper_test.go
@@ -86,5 +86,5 @@ func TestFormatAndValidateCreatedLibrary(t *testing.T) {
 		t.Errorf("formatting should have given us 6 lines but got: %d", lineCount)
 	}
 	command.Run(t.Context(), "git", "restore", "--source=HEAD", "--staged", "--worktree", "./testdata")
-	command.Run(t.Context(), "git", "clean", "-fd", "./testdata")
+	command.Run(t.Context(), "git", "clean", "-fd", ".")
 }
