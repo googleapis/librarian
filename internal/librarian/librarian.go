694e166897637cbc112ae3c412bb7713cda210a6
Min Zhu <zhumin@google.com>
2025-12-16 17:12:06 -0500

    fix lint issues

--- internal/librarian/librarian.go
+++ internal/librarian/librarian.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package librarian provides functionality for onboarding, generating and
-// releasing Google Cloud client libraries.
 package librarian
 
 import (
--- internal/librarian/publish_test.go
+++ internal/librarian/publish_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	https://www.apache.org/licenses/LICENSE-2.0
+//     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -80,7 +80,7 @@ func TestPublish(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			tmpDir := t.TempDir()
 			t.Chdir(tmpDir)
-			testhelpers.SetupRepo(t, "v1.0.0")
+			testhelpers.SetupRepo(t.Context(), t, "v1.0.0")
 
 			var (
 				preflightCalled bool
