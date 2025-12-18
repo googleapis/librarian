34c7553a6e942ad0c335a8fdad4f26dabad11a28
Santiago Quiroga <santiquiroga@google.com>
2025-12-17 20:48:55 -0600

    feat(surfer): restructure generator implementation

--- internal/surfer/gcloud/builder.go
+++ internal/surfer/gcloud/builder.go
@@ -1,4 +1,5 @@
 // Copyright 2025 Google LLC
+
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -398,6 +399,7 @@ func findFieldHelpTextRule(field *api.Field, overrides *Config) *HelpTextRule {
 func apiVersion(overrides *Config) string {
 	if len(overrides.APIs) > 0 {
 		return overrides.APIs[0].APIVersion
+
 	}
 	return ""
 }
