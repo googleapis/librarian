19da6202e34db6cd1cd4f9009193a8c44e0a6655
Santiago Quiroga <santiquiroga@google.com>
2025-12-18 09:51:15 -0600

    chore(internal/surfer): fix linting spaces

--- internal/surfer/gcloud/utils/field_test.go
+++ internal/surfer/gcloud/utils/field_test.go
@@ -45,5 +45,3 @@ func TestGetGcloudType(t *testing.T) {
 		})
 	}
 }
-
-
--- internal/surfer/gcloud/utils/resource_test.go
+++ internal/surfer/gcloud/utils/resource_test.go
@@ -21,8 +21,6 @@ import (
 	"github.com/googleapis/librarian/internal/sidekick/api"
 )
 
-
-
 func TestGetPluralFromSegments(t *testing.T) {
 	for _, test := range []struct {
 		name     string
