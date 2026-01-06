f9aad86f411fe361403547ee212652405cb9a967
Min Zhu <zhumin@google.com>
2026-01-06 11:59:23 -0500

    test(internal/fetch): use localhost for invalid URL tests

--- internal/fetch/fetch_test.go
+++ internal/fetch/fetch_test.go
@@ -117,7 +117,7 @@ func TestSha256Error(t *testing.T) {
 		},
 		{
 			name: "invalid url",
-			url:  newClosedServerURL(t),
+			url:  "http://127.0.0.1:54321",
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
@@ -175,7 +175,7 @@ func TestLatestShaError(t *testing.T) {
 		},
 		{
 			name: "invalid url",
-			url:  newClosedServerURL(t),
+			url:  "http://127.0.0.1:54321",
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
@@ -733,11 +733,4 @@ func TestLatestCommitAndChecksumFailure(t *testing.T) {
 			t.Error("expected an error when Sha256 fails, but got nil")
 		}
 	})
-}
-
-func newClosedServerURL(t *testing.T) string {
-	t.Helper()
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
-	server.Close()
-	return server.URL
 }
