09a493e615ad0b185e9352a92d254c4b89e4e6d4
Min Zhu <zhumin@google.com>
2026-01-07 15:47:10 -0500

    Revert "fix(internal/yaml): omit nil and empty string slices (#3434)"
    This reverts commit 70fe7434340f021acd94fd8c5cb7614068603a74.

--- internal/config/language.go
+++ internal/config/language.go
@@ -14,6 +14,8 @@
 
 package config
 
+import "github.com/googleapis/librarian/internal/yaml"
+
 // GoModule represents the Go-specific configuration for a library.
 type GoModule struct {
 	DeleteGenerationOutputPaths []string `yaml:"delete_generation_output_paths,omitempty"`
@@ -47,7 +49,7 @@ type RustDefault struct {
 // and where to output the generated code.
 type RustModule struct {
 	// DisabledRustdocWarnings is a list of rustdoc warnings to disable.
-	DisabledRustdocWarnings []string `yaml:"disabled_rustdoc_warnings,omitempty"`
+	DisabledRustdocWarnings yaml.StringSlice `yaml:"disabled_rustdoc_warnings,omitempty"`
 
 	// DocumentationOverrides contains overrides for element documentation.
 	DocumentationOverrides []RustDocumentationOverride `yaml:"documentation_overrides,omitempty"`
