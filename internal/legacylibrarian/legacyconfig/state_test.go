a45b9ab833ff94b8aa4b43a1223268082c20bdc9
Noah Dietz <ndietz@google.com>
2026-01-27 19:18:06 +0000

    chore(legacylibrarian): unit test library state version string

--- internal/legacylibrarian/legacyconfig/state_test.go
+++ internal/legacylibrarian/legacyconfig/state_test.go
@@ -186,6 +186,36 @@ func TestLibrary_Validate(t *testing.T) {
 			},
 		},
 		{
+			name: "invalid version no prerelease separator",
+			library: &LibraryState{
+				ID:          "a/b",
+				Version:     "1.2.3dev0",
+				SourceRoots: []string{"src/a", "src/b"},
+				APIs: []*API{
+					{
+						Path: "a/b/v1",
+					},
+				},
+			},
+			wantErr:    true,
+			wantErrMsg: "invalid version",
+		},
+		{
+			name: "invalid version bad prerelease separator",
+			library: &LibraryState{
+				ID:          "a/b",
+				Version:     "1.2.3.dev0",
+				SourceRoots: []string{"src/a", "src/b"},
+				APIs: []*API{
+					{
+						Path: "a/b/v1",
+					},
+				},
+			},
+			wantErr:    true,
+			wantErrMsg: "invalid version",
+		},
+		{
 			name: "invalid id characters",
 			library: &LibraryState{
 				ID: "a/b!",
