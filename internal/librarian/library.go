f7713e2fc2c4c043c9a5c9836e11d40d7157ffb9
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-01-30 14:33:59 -0500

    fill dart packages

--- internal/librarian/library.go
+++ internal/librarian/library.go
@@ -82,6 +82,9 @@ func fillDart(lib *config.Library, d *config.Default) *config.Library {
 	if lib.Dart.Dependencies == "" {
 		lib.Dart.Dependencies = d.Dart.Dependencies
 	}
+	if lib.Dart.Packages == nil {
+		lib.Dart.Packages = d.Dart.Packages
+	}
 
 	return lib
 }
