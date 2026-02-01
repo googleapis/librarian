5c51710562eddb0c7e16e1eec4231677a370137d
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-01-31 19:18:26 -0500

    chore: add librarian binary version

--- tool/cmd/migrate/main_test.go
+++ tool/cmd/migrate/main_test.go
@@ -37,6 +37,7 @@ func TestReadRootSidekick(t *testing.T) {
 			path: "testdata/root-sidekick/success",
 			want: &config.Config{
 				Language: "dart",
+				Version:  "not available",
 				Sources: &config.Sources{
 					Googleapis: &config.Source{
 						Commit: "211d22fa6dfabfa52cbda04d1aee852a01301edf",
