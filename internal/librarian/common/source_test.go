562154ef8c742503e3adfa3bdc5bf016897983dc
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-02-02 16:37:57 -0500

    add unit tests

--- internal/librarian/common/source_test.go
+++ internal/librarian/common/source_test.go
@@ -21,9 +21,78 @@ import (
 	"github.com/googleapis/librarian/internal/config"
 )
 
+func TestAddLibraryRoots(t *testing.T) {
+	for _, test := range []struct {
+		name    string
+		library *config.Library
+		source  *Sources
+		want    map[string]string
+	}{
+		{
+			name:    "empty roots",
+			library: &config.Library{},
+			source: &Sources{
+				Googleapis: "example/path",
+			},
+			want: map[string]string{
+				"googleapis-root": "example/path",
+				"roots":           "googleapis",
+			},
+		},
+		{
+			name: "non existed sources",
+			library: &config.Library{
+				Roots: []string{"non-existed", "googleapis"},
+			},
+			source: &Sources{
+				Googleapis:  "example/path",
+				ProtobufSrc: "protobuf/path",
+			},
+			want: map[string]string{
+				"googleapis-root": "example/path",
+				"roots":           "non-existed,googleapis",
+			},
+		},
+		{
+			name: "all sources",
+			library: &config.Library{
+				Roots: []string{
+					"conformance",
+					"discovery",
+					"googleapis",
+					"protobuf-src",
+					"showcase",
+				},
+			},
+			source: &Sources{
+				Conformance: "conformance/path",
+				Discovery:   "discovery/path",
+				Googleapis:  "googleapis/path",
+				ProtobufSrc: "protobuf/path",
+				Showcase:    "showcase/path",
+			},
+			want: map[string]string{
+				"conformance-root":  "conformance/path",
+				"discovery-root":    "discovery/path",
+				"googleapis-root":   "googleapis/path",
+				"protobuf-src-root": "protobuf/path",
+				"showcase-root":     "showcase/path",
+				"roots":             "conformance,discovery,googleapis,protobuf-src,showcase",
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			got := AddLibraryRoots(test.library, test.source)
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
 func TestFetchSource(t *testing.T) {
 	ctx := t.Context()
-	for _, tt := range []struct {
+	for _, test := range []struct {
 		name    string
 		source  *config.Source
 		wantDir string
@@ -40,13 +109,13 @@ func TestFetchSource(t *testing.T) {
 			wantDir: "local/dir",
 		},
 	} {
-		t.Run(tt.name, func(t *testing.T) {
-			gotDir, err := fetchSource(ctx, tt.source, "some-repo")
-			if (err != nil) != tt.wantErr {
-				t.Errorf("fetchSource() error = %v, wantErr %v", err, tt.wantErr)
+		t.Run(test.name, func(t *testing.T) {
+			gotDir, err := fetchSource(ctx, test.source, "some-repo")
+			if (err != nil) != test.wantErr {
+				t.Errorf("fetchSource() error = %v, wantErr %v", err, test.wantErr)
 				return
 			}
-			if diff := cmp.Diff(tt.wantDir, gotDir); diff != "" {
+			if diff := cmp.Diff(test.wantDir, gotDir); diff != "" {
 				t.Errorf("mismatch (-want +got):\n%s", diff)
 			}
 		})
