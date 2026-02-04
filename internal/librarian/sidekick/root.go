860c2c9d33a0d019d2d830bb27f38d3c45f65065
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-02-04 10:47:25 -0500

    update comments

--- internal/librarian/sidekick/root.go
+++ internal/librarian/sidekick/root.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package internalsidekick provides functionalities for Dart and Rust generation and releasing.
 package internalsidekick
 
 import (
--- internal/librarian/source/source.go
+++ internal/librarian/source/source.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package source provides common functionalities for generating and releasing clients in all languages.
+// Package source provides functionalities to fetch and process source for generating and releasing clients in all
+// languages.
 package source
 
 import (
