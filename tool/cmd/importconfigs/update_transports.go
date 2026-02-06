cf5baf8c33b0ecde349741920ad559022d6a0998
Min Zhu <zhumin@google.com>
2026-02-06 11:51:34 -0500

    fix: add update-transport as a new tool command
    mv update_transports.go to tool/cmd and inline command logic

--- tool/cmd/importconfigs/update_transports.go
+++ tool/cmd/importconfigs/update_transports.go
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package importconfig provides commands for importing configurations.
-package importconfig
+package main
 
 import (
 	"context"
