3fc7cb496562dadf6af1a63433b67c1248f5f057
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-02-09 14:38:11 -0500

    add comments

--- internal/serviceconfig/api.go
+++ internal/serviceconfig/api.go
@@ -81,6 +81,9 @@ type API struct {
 	Transports map[string]Transport
 }
 
+// GetTransport gets transport for a given language.
+//
+// The default value is GRPCRest, if unset for a language.
 func (api *API) GetTransport(language string) string {
 	if trans, ok := api.Transports[language]; ok {
 		return string(trans)
