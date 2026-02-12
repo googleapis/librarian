bf117e0f3929d048c5459a9a48321f590a635292
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-02-12 15:21:27 -0500

    refactor

--- tool/cmd/migrate/legacylibrarian.go
+++ tool/cmd/migrate/legacylibrarian.go
@@ -243,8 +243,7 @@ func buildGoLibraries(input *MigrationInput) ([]*config.Library, error) {
 		if libState.APIs != nil {
 			library.APIs = toAPIs(libState.APIs)
 		}
-		library.Keep = append(library.Keep, libState.PreserveRegex...)
-		library.Keep = append(library.Keep, "CHANGES.md")
+		library.Keep = append(library.Keep, append(libState.PreserveRegex, "CHANGES.md")...)
 		if libraryNames[id] {
 			library.Keep = append(library.Keep, filepath.Join(id, "aliasshim", "aliasshim.go"))
 		}
