ec7465cbfc327f8cae13b3765dc156262a78137b
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-02-17 17:34:27 -0500

    refactor(internal/librarian/golang): refactor version functions in Go  (#4059)
    Create functions to calculate client directory and versioned api path
    for Go package.
    
    For #3617

---
+++ internal/librarian/golang/module_test.go
@@ -0,0 +1,80 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package golang
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/googleapis/librarian/internal/config"
+)
+
+func TestFindGoAPI(t *testing.T) {
+	for _, test := range []struct {
+		name    string
+		library *config.Library
+		apiPath string
+		want    *config.GoAPI
+	}{
+		{
+			name: "find an api",
+			library: &config.Library{
+				Name: "secretmanager",
+				Go: &config.GoModule{
+					GoAPIs: []*config.GoAPI{
+						{
+							Path:            "google/cloud/secretmanager/v1",
+							ClientDirectory: "customDir",
+						},
+					},
+				},
+			},
+			apiPath: "google/cloud/secretmanager/v1",
+			want: &config.GoAPI{
+				Path:            "google/cloud/secretmanager/v1",
+				ClientDirectory: "customDir",
+			},
+		},
+		{
+			name: "do not have a go module",
+			library: &config.Library{
+				Name: "secretmanager",
+			},
+			apiPath: "google/cloud/secretmanager/v1",
+		},
+		{
+			name: "find an api",
+			library: &config.Library{
+				Name: "secretmanager",
+				Go: &config.GoModule{
+					GoAPIs: []*config.GoAPI{
+						{
+							Path:            "google/cloud/secretmanager/v1",
+							ClientDirectory: "customDir",
+						},
+					},
+				},
+			},
+			apiPath: "google/cloud/secretmanager/v1beta1",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			got := findGoAPI(test.library, test.apiPath)
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
