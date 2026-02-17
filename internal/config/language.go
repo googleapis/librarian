9f6f3dc3c5d6892f66cc4658b92877b174620485
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-02-17 16:17:15 -0500

    add comments

--- internal/config/language.go
+++ internal/config/language.go
@@ -18,20 +18,32 @@ import "github.com/googleapis/librarian/internal/yaml"
 
 // GoModule represents the Go-specific configuration for a library.
 type GoModule struct {
+	// DeleteGenerationOutputPaths is a list of paths to delete before generation.
 	DeleteGenerationOutputPaths []string `yaml:"delete_generation_output_paths,omitempty"`
-	GoAPIs                      []*GoAPI `yaml:"go_apis,omitempty"`
-	ModulePathVersion           string   `yaml:"module_path_version,omitempty"`
+	// GoAPIs is a list of Go-specific API configurations.
+	GoAPIs []*GoAPI `yaml:"go_apis,omitempty"`
+	// ModulePathVersion is the version of the Go module path.
+	ModulePathVersion string `yaml:"module_path_version,omitempty"`
 }
 
 // GoAPI represents configuration for a single API within a Go module.
 type GoAPI struct {
-	ClientDirectory    string   `yaml:"client_directory,omitempty"`
-	DisableGAPIC       bool     `yaml:"disable_gapic,omitempty"`
-	ImportPath         string   `yaml:"import_path,omitempty"`
-	NestedProtos       []string `yaml:"nested_protos,omitempty"`
-	NoRESTNumericEnums bool     `yaml:"no_rest_numeric_enums,omitempty"`
-	Path               string   `yaml:"path,omitempty"`
-	ProtoPackage       string   `yaml:"proto_package,omitempty"`
+	// ClientDirectory is the directory where the client is generated, relative to Library.Output.
+	ClientDirectory string `yaml:"client_directory,omitempty"`
+	// DisableGAPIC determines whether to generate the GAPIC client.
+	DisableGAPIC bool `yaml:"disable_gapic,omitempty"`
+	// ImportPath is the Go import path for the API.
+	ImportPath string `yaml:"import_path,omitempty"`
+	// NestedProtos is a list of nested proto files.
+	NestedProtos []string `yaml:"nested_protos,omitempty"`
+	// NoRESTNumericEnums determines whether to use numeric enums in REST requests.
+	// Use No prefix because we want the default value to be `false`, otherwise lots of libraries will
+	// have an additional configuration.
+	NoRESTNumericEnums bool `yaml:"no_rest_numeric_enums,omitempty"`
+	// Path is the source path.
+	Path string `yaml:"path,omitempty"`
+	// ProtoPackage is the proto package name.
+	ProtoPackage string `yaml:"proto_package,omitempty"`
 }
 
 // RustDefault contains Rust-specific default configuration.
