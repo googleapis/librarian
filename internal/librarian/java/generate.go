db573d2232272f851959631c3da8b1e920f80bb0
Min Zhu <zhumin@google.com>
2026-02-20 16:31:14 -0500

    add java/generate skeleton

---
+++ internal/librarian/java/generate.go
@@ -0,0 +1,63 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package java provides Java specific functionality for librarian.
+package java
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/googleapis/librarian/internal/config"
+)
+
+// GenerateLibraries generates all the given libraries in sequence.
+func GenerateLibraries(ctx context.Context, libraries []*config.Library, googleapisDir string) error {
+	for _, library := range libraries {
+		if err := generate(ctx, library, googleapisDir); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// generate generates a Java client library.
+func generate(ctx context.Context, library *config.Library, googleapisDir string) error {
+	if len(library.APIs) == 0 {
+		return fmt.Errorf("no apis configured for library %q", library.Name)
+	}
+	fmt.Printf("to be implemented with: %v, %v, %v", ctx, library.Name, googleapisDir)
+	return nil
+}
+
+// FormatLibraries formats all given libraries in sequence.
+func FormatLibraries(ctx context.Context, libraries []*config.Library) error {
+	for _, library := range libraries {
+		if err := Format(ctx, library); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Format formats a Java client library using google-java-format.
+func Format(ctx context.Context, library *config.Library) error {
+	return nil
+}
+
+// Clean removes files in the library's output directory that are not in the keep list.
+// It targets patterns like proto-*, grpc-*, and the main GAPIC module.
+func Clean(library *config.Library) error {
+	return nil
+}
