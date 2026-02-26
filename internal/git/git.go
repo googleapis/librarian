354328c0ea6e210c05d8eb7a5d834da275c802e2
Julie Qiu <julie@golang.org>
2026-02-26 10:15:34 -0500

    fix(internal/git): move %w to end of error format string
    The wrapped error should appear at the end of the fmt.Errorf format
    string, following Go conventions.

--- internal/git/git.go
+++ internal/git/git.go
@@ -135,7 +135,7 @@ func ShowFileAtRevision(ctx context.Context, gitExe, revision, path string) (str
 	revisionAndPath := fmt.Sprintf("%s:%s", revision, path)
 	output, err := command.Output(ctx, gitExe, "show", revisionAndPath)
 	if err != nil {
-		return "", errors.Join(fmt.Errorf("%w: %s", errGitShow, revisionAndPath), err)
+		return "", errors.Join(fmt.Errorf("%s: %w", revisionAndPath, errGitShow), err)
 	}
 	return strings.TrimSuffix(output, "\n"), nil
 }
