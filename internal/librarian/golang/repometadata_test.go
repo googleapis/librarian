b395de363a8668c738e10e7d0fa5f134fb2aee27
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-03-03 15:34:41 -0500

    fix: use module path version to build client doc

--- internal/librarian/golang/repometadata_test.go
+++ internal/librarian/golang/repometadata_test.go
@@ -168,22 +168,29 @@ func TestGenerateRepoMetadata_Error(t *testing.T) {
 func TestGoClientDocURL(t *testing.T) {
 	for _, test := range []struct {
 		name       string
+		library    *config.Library
 		importPath string
 		want       string
 	}{
 		{
-			name:       "basic",
+			name: "basic",
+			library: &config.Library{
+				Name: "secretmanager",
+			},
 			importPath: "secretmanager/apiv1",
 			want:       "https://cloud.google.com/go/docs/reference/cloud.google.com/go/secretmanager/latest/apiv1",
 		},
 		{
-			name:       "spanner",
+			name: "spanner",
+			library: &config.Library{
+				Name: "spanner",
+			},
 			importPath: "spanner/admin/database/apiv1",
 			want:       "https://cloud.google.com/go/docs/reference/cloud.google.com/go/spanner/latest/admin/database/apiv1",
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
-			got := clientDocURL(test.importPath)
+			got := clientDocURL(test.library, test.importPath)
 			if diff := cmp.Diff(test.want, got); diff != "" {
 				t.Errorf("mismatch (-want +got):\n%s", diff)
 			}
