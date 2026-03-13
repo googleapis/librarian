4b912e90ec016b98f4b06bd55948ab01a25d6d63
haphung <haphung@google.com>
2026-03-13 23:12:03 +0000

    refactor HasBindings from variable to function

--- internal/sidekick/rust/annotate_method_test.go
+++ internal/sidekick/rust/annotate_method_test.go
@@ -87,7 +87,7 @@ func TestAnnotateMethodNames(t *testing.T) {
 			continue
 		}
 		got := gotMethod.Codec.(*methodAnnotation)
-		if diff := cmp.Diff(test.Want, got, cmpopts.IgnoreFields(methodAnnotation{}, "PathInfo", "SystemParameters", "HasBindings")); diff != "" {
+		if diff := cmp.Diff(test.Want, got, cmpopts.IgnoreFields(methodAnnotation{}, "PathInfo", "SystemParameters")); diff != "" {
 			t.Errorf("mismatch (-want, +got):\n%s", diff)
 		}
 	}
@@ -418,7 +418,7 @@ func TestAnnotateMethodResourceNameTemplate(t *testing.T) {
 				"SystemParameters", "ReturnType", "PathInfo", "Attributes",
 				"RoutingRequired", "DetailedTracingAttributes",
 				"ResourceNameTemplateGrpc", "GrpcResourceNameArgs", "HasGrpcResourceNameArgs",
-				"InternalBuilders", "HasBindings")); diff != "" {
+				"InternalBuilders")); diff != "" {
 				t.Errorf("mismatch (-want, +got):\n%s", diff)
 			}
 
