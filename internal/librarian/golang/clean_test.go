7b54cd2750d3f075cbc3a01ad89fdb1c7e3c36c1
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-03-13 20:54:31 -0400

    update unit tests

--- internal/librarian/golang/clean_test.go
+++ internal/librarian/golang/clean_test.go
@@ -290,7 +290,7 @@ func TestClean_Error(t *testing.T) {
 			root := t.TempDir()
 			outputPath := filepath.Join(root, libraryName)
 			snippetPath := filepath.Join(root, "internal", "generated", "snippets", libraryName)
-			test.library.Output = root
+			test.library.Output = outputPath
 			if test.outputFiles != nil {
 				createFiles(t, outputPath, test.outputFiles)
 			}
