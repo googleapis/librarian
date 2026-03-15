7d20126c9bcbc9c5b4a2df58f5e9396d1e91b847
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-03-14 23:23:08 -0400

    refactor

--- internal/librarian/golang/tidy.go
+++ internal/librarian/golang/tidy.go
@@ -23,9 +23,7 @@ import (
 // Tidy tidies the Go-specific configuration for a library by removing default
 // values and clearing empty Go module or API entries.
 func Tidy(library *config.Library) *config.Library {
-	if strings.HasSuffix(library.Output, library.Name) {
-		library.Output = strings.TrimSuffix(library.Output, library.Name)
-	}
+	library.Output = strings.TrimSuffix(library.Output, library.Name)
 	if library.Go == nil {
 		return library
 	}
