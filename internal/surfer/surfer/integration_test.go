413db4ff05953fd9c79b9c58612506883417140c
Santiago Quiroga <santiquiroga@google.com>
2026-03-22 18:17:36 -0700

    chore(internal/surfer): fix tests

--- internal/surfer/surfer/integration_test.go
+++ internal/surfer/surfer/integration_test.go
@@ -1,10 +1,22 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package surfer
 
 import (
 	"context"
 	"flag"
-	"github.com/google/go-cmp/cmp"
-	"gopkg.in/yaml.v3"
 	"io/fs"
 	"os"
 	"os/exec"
@@ -12,6 +24,9 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"gopkg.in/yaml.v3"
 )
 
 var (
@@ -193,7 +208,7 @@ func copyProtos(t *testing.T, src, dst string) {
 	}
 }
 
-func findProtos(t *testing.T, root string) []string {
+func findProtos(_ *testing.T, root string) []string {
 	var protos []string
 	filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
