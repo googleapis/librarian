58091376504f0a05fd7d82dbd9dc2572da4eab61
Min Zhu <zhumin@google.com>
2026-03-26 10:56:14 -0400

    update test to use sample.RepoMetadata()

--- internal/librarian/java/repometadata_test.go
+++ internal/librarian/java/repometadata_test.go
@@ -21,24 +21,26 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/googleapis/librarian/internal/config"
+	"github.com/googleapis/librarian/internal/sample"
 )
 
 func TestRepoMetadata_write(t *testing.T) {
+	s := sample.RepoMetadata()
 	want := &repoMetadata{
-		APIShortname:         "secretmanager",
-		NamePretty:           "Secret Manager",
-		ProductDocumentation: "https://cloud.google.com/secret-manager/",
-		APIDescription:       "Stores sensitive data such as API keys, passwords, and certificates. Provides convenience while improving security.",
+		APIShortname:         s.APIShortname,
+		NamePretty:           s.NamePretty,
+		ProductDocumentation: s.ProductDocumentation,
+		APIDescription:       s.APIDescription,
 		ClientDocumentation:  "https://cloud.google.com/java/docs/reference/google-cloud-secretmanager/latest/overview",
-		ReleaseLevel:         "stable",
+		ReleaseLevel:         s.ReleaseLevel,
 		Transport:            "grpc",
 		Language:             "java",
 		Repo:                 "googleapis/google-cloud-java",
 		RepoShort:            "java-secretmanager",
 		DistributionName:     "com.google.cloud:google-cloud-secretmanager",
-		LibraryType:          "GAPIC_AUTO",
+		LibraryType:          s.LibraryType,
 		CodeownerTeam:        "cloud-java-team",
-		IssueTracker:         "https://issuetracker.google.com/issues/new?component=187210&template=0",
+		IssueTracker:         s.IssueTracker,
 		RestDocumentation:    "https://example.com/rest",
 		RpcDocumentation:     "https://example.com/rpc",
 		RecommendedPackage:   "com.google.cloud.secretmanager.v1",
@@ -64,7 +66,7 @@ func TestRepoMetadata_write(t *testing.T) {
   "api_shortname": "secretmanager",
   "name_pretty": "Secret Manager",
   "product_documentation": "https://cloud.google.com/secret-manager/",
-  "api_description": "Stores sensitive data such as API keys, passwords, and certificates. Provides convenience while improving security.",
+  "api_description": "Stores sensitive data such as API keys, passwords, and certificates.\nProvides convenience while improving security.",
   "client_documentation": "https://cloud.google.com/java/docs/reference/google-cloud-secretmanager/latest/overview",
   "release_level": "stable",
   "transport": "grpc",
@@ -75,7 +77,7 @@ func TestRepoMetadata_write(t *testing.T) {
   "library_type": "GAPIC_AUTO",
   "requires_billing": false,
   "codeowner_team": "cloud-java-team",
-  "issue_tracker": "https://issuetracker.google.com/issues/new?component=187210\u0026template=0",
+  "issue_tracker": "https://issuetracker.google.com/issues/new?component=784854\u0026template=1380926",
   "rest_documentation": "https://example.com/rest",
   "rpc_documentation": "https://example.com/rpc",
   "recommended_package": "com.google.cloud.secretmanager.v1",
@@ -91,10 +93,9 @@ func TestDeriveRepoMetadata_Overrides(t *testing.T) {
 	apiPath := "google/cloud/secretmanager/v1"
 	googleapis := "internal/testdata/googleapis"
 
-	cfg := &config.Config{
-		Language: config.LanguageJava,
-		Repo:     "googleapis/google-cloud-java",
-	}
+	cfg := sample.Config()
+	cfg.Language = config.LanguageJava
+	cfg.Repo = "googleapis/google-cloud-java"
 	library := &config.Library{
 		Name: "secretmanager",
 		APIs: []*config.API{{Path: apiPath}},
@@ -114,15 +115,16 @@ func TestDeriveRepoMetadata_Overrides(t *testing.T) {
 	if err != nil {
 		t.Fatalf("deriveRepoMetadata failed: %v", err)
 	}
+	s := sample.RepoMetadata()
 	want := &repoMetadata{
 		NamePretty:           "Custom Pretty Name",
 		ProductDocumentation: "https://custom.docs",
 		APIDescription:       "Custom description",
 		ClientDocumentation:  "https://custom.client.docs",
-		ReleaseLevel:         "",
+		ReleaseLevel:         s.ReleaseLevel,
 		Transport:            "both",
-		Language:             "java",
-		Repo:                 "googleapis/google-cloud-java",
+		Language:             cfg.Language,
+		Repo:                 cfg.Repo,
 		RepoShort:            "java-secretmanager",
 		DistributionName:     "com.custom:custom-artifact",
 		APIID:                "custom.googleapis.com",
