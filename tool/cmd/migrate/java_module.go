7c6db835c5eaf073f8cc85fff00be8dfad902f48
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-04-10 22:30:55 -0400

    add api shortname override

--- tool/cmd/migrate/java_module.go
+++ tool/cmd/migrate/java_module.go
@@ -2,6 +2,13 @@ package main
 
 var (
 	apiShortNameOverride = map[string]string{
-		"beyondcorp-appconnections": "beyondcorp-appconnections",
+		"beyondcorp-appconnections":          "beyondcorp-appconnections",
+		"beyondcorp-appconnectors":           "beyondcorp-appconnectors",
+		"beyondcorp-appgateways":             "beyondcorp-appgateways",
+		"beyondcorp-clientconnectorservices": "beyondcorp-clientconnectorservices",
+		"beyondcorp-clientgateways":          "beyondcorp-clientgateways",
+		"dialogflow-cx":                      "dialogflow-cx",
+		"distributedcloudedge":               "distributedcloudedge",
+		"gke-backup":                         "gke-backup",
 	}
 )
