b9dd2f3e17f11498cd09dbc45ebd249f7bcaa7c5
Sofia Leon <sofialeon@google.com>
2026-04-15 21:21:36 +0000

    readd blank space

--- internal/librarian/golang/generate.go
+++ internal/librarian/golang/generate.go
@@ -73,6 +73,7 @@ func Generate(ctx context.Context, library *config.Library, srcs *sources.Source
 		if goAPI == nil {
 			return fmt.Errorf("error finding goAPI associated with API %s: %w", api.Path, errGoAPINotFound)
 		}
+		
 		if err := generateAPI(ctx, goAPI, googleapisDir, library.Version, tempDir); err != nil {
 			return fmt.Errorf("api %q: %w", api.Path, err)
 		}
