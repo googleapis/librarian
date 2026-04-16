8dfb8a698067bc4cda4ab46ce6f6c510c2c200df
Sofia Leon <sofialeon@google.com>
2026-04-16 00:50:08 +0000

    chore: fix duplicate package godoc in dotnotation.go

--- internal/yaml/dotnotation.go
+++ internal/yaml/dotnotation.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package yaml provides utilities to interact with librarian configurations.
 package yaml
 
 import (
