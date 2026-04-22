2c1b002fcf30ec5518020427dacf0b8faf317e1b
Min Zhu <zhumin@google.com>
2026-04-21 22:31:49 -0400

    use cmp.Diff in test

--- internal/librarian/java/add_test.go
+++ internal/librarian/java/add_test.go
@@ -51,8 +51,8 @@ func TestDefaultLibraryName(t *testing.T) {
 	} {
 		t.Run(test.api, func(t *testing.T) {
 			got := DefaultLibraryName(test.api)
-			if got != test.want {
-				t.Errorf("DefaultLibraryName(%q) = %q, want %q", test.api, got, test.want)
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
