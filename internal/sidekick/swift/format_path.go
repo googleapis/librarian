f3eb67d595c3e52216a944069ab995837b4cbc61
Carlos O'Ryan <coryan@google.com>
2026-04-23 12:13:51 -0400

    feat(sidekick/swift): handle optional path parameters
    Sometimes the fields used to build the URL path are optional, or are nested
    within optional messages. The generator needs to emit code to detect if the
    field is not set, and if so, throw an error.

--- internal/sidekick/swift/format_path.go
+++ internal/sidekick/swift/format_path.go
@@ -21,18 +21,79 @@ import (
 	"github.com/googleapis/librarian/internal/sidekick/api"
 )
 
-func formatPath(t *api.PathTemplate) string {
+func pathExpression(t *api.PathTemplate) string {
+	count := 0
 	var pathComponents []string
 	for _, segment := range t.Segments {
 		if segment.Literal != nil {
 			pathComponents = append(pathComponents, *segment.Literal)
 		} else if segment.Variable != nil {
-			var fieldParts []string
-			for _, p := range segment.Variable.FieldPath {
-				fieldParts = append(fieldParts, camelCase(p))
-			}
-			pathComponents = append(pathComponents, fmt.Sprintf(`\(request.%s)`, strings.Join(fieldParts, ".")))
+			pathComponents = append(pathComponents, fmt.Sprintf(`\(pathVariable%d)`, count))
+			count += 1
 		}
 	}
 	return "/" + strings.Join(pathComponents, "/")
+}
+
+func (c *codec) pathVariables(message *api.Message, t *api.PathTemplate) ([]*pathVariable, error) {
+	count := 0
+	var variables []*pathVariable
+	for _, segment := range t.Segments {
+		if segment.Variable != nil {
+			new, err := c.newPathVariable(message, segment.Variable, count)
+			if err != nil {
+				return nil, err
+			}
+			variables = append(variables, new)
+			count += 1
+		}
+	}
+	return variables, nil
+}
+
+func (c *codec) newPathVariable(message *api.Message, variable *api.PathVariable, count int) (*pathVariable, error) {
+	test := ""
+	name := fmt.Sprintf("pathVariable%d", count)
+	var expression strings.Builder
+	optional := false
+	current := message
+	for _, v := range variable.FieldPath {
+		field, err := lookupField(current, v)
+		if err != nil {
+			return nil, err
+		}
+		fieldCodec, ok := field.Codec.(*fieldAnnotations)
+		if !ok {
+			return nil, fmt.Errorf("internal error: field %s in message %s does not have swift fieldAnnotations", field.Name, current.ID)
+		}
+		if optional && field.Optional {
+			fmt.Fprintf(&expression, ".flatMap({ $0.%s })", fieldCodec.Name)
+		} else if optional {
+			fmt.Fprintf(&expression, ".map({ $0.%s })", fieldCodec.Name)
+		} else if field.Optional {
+			fmt.Fprintf(&expression, ".%s", fieldCodec.Name)
+		} else {
+			fmt.Fprintf(&expression, ".%s as %s?", fieldCodec.Name, fieldCodec.FieldType)
+		}
+		optional = field.Optional
+		switch field.Typez {
+		case api.TypezMessage:
+			current, err = lookupMessage(c.Model, field.TypezID)
+			if err != nil {
+				return nil, err
+			}
+		case api.TypezString:
+			test = fmt.Sprintf("!%s.isEmpty", name)
+		case api.TypezBytes:
+			return nil, fmt.Errorf("unsupported path parameter type %q, message=%q, path=%q", field.Typez.String(), message.ID, variable.FieldPath)
+		default:
+			test = ""
+		}
+	}
+	new := &pathVariable{
+		Name:       name,
+		Expression: expression.String(),
+		Test:       test,
+	}
+	return new, nil
 }
