745bbf5e582ab0b9744aa2a789daeabc3a820bd9
Sarah Heacock <sheacock@google.com>
2026-04-24 20:21:21 +0000

    enable multi version multi track

--- internal/surfer/golden_file_test.go
+++ internal/surfer/golden_file_test.go
@@ -70,7 +70,7 @@ func TestGolden(t *testing.T) {
 		{name: "method_operations"},
 		{name: "method_output_format"},
 		{name: "multi_service"},
-		{name: "multi_version_multi_track", skip: "fails against autogen target"},
+		{name: "multi_version_multi_track"},
 		{name: "regional_endpoints/global_only"},
 		{name: "regional_endpoints/regional_required"},
 		{name: "regional_endpoints/regional_supported"},
