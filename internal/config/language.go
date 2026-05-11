1c9bfeb35f06f9213317b72e9f909c02624c9390
Min Zhu <zhumin@google.com>
2026-05-11 14:00:23 -0400

    update comment

--- internal/config/language.go
+++ internal/config/language.go
@@ -498,7 +498,8 @@ type JavaModule struct {
 	// ExcludedDependencies is a list of dependencies to exclude.
 	ExcludedDependencies string `yaml:"excluded_dependencies,omitempty"`
 
-	// ExcludedPOMs is a list of POM files to exclude.
+	// ExcludedPOMs is a list of artifact ids, whose module should be excluded
+	// when updating pom.xml and are omitted when counting new modules.
 	ExcludedPOMs []string `yaml:"excluded_poms,omitempty"`
 
 	// ExtraVersionedModules is a list of extra versioned modules.
