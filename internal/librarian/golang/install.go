891e611022e207c426c7836407aca560ffebd6b9
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-05-28 15:12:46 -0400

    feat: set bin dir for go tools

--- internal/librarian/golang/install.go
+++ internal/librarian/golang/install.go
@@ -18,9 +18,16 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/googleapis/librarian/internal/command"
 	"github.com/googleapis/librarian/internal/config"
+)
+
+const (
+	goBinEnvVar = "GOBIN"
+	librarianBinEnvVar = "LIBRARIAN_INSTALL_DIR"
 )
 
 // errMissingToolVersion indicates a go tool entry is missing its version.
@@ -38,14 +45,36 @@ func Install(ctx context.Context, tools *config.Tools) error {
 }
 
 func installGoTools(ctx context.Context, goTools []*config.GoTool) error {
+	installDir, err := getInstallDir()
+	if err != nil {
+		return err
+	}
+	// Go uses the GOBIN environment variable to determine where to install tools,
+	// and it does not allow to specify the installation directory as a command-line argument.
+	// We set it for the duration of the installation.
+	originalGOBIN := os.Getenv(goBinEnvVar)
+	defer os.Setenv(goBinEnvVar, originalGOBIN)
+	env := map[string]string{goBinEnvVar: installDir}
 	for _, tool := range goTools {
 		if tool.Version == "" {
 			return fmt.Errorf("%w: %s", errMissingToolVersion, tool.Name)
 		}
 		toolStr := fmt.Sprintf("%s@%s", tool.Name, tool.Version)
-		if err := command.Run(ctx, command.Go, "install", toolStr); err != nil {
+		if err := command.RunWithEnv(ctx, env, command.Go, "install", toolStr); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func getInstallDir() (string, error) {
+	dir := os.Getenv(librarianBinEnvVar)
+	if dir == "" {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", fmt.Errorf("failed to get user home directory: %w", err)
+		}
+		dir = filepath.Join(home, "go_tools", "bin")
+	}
+	return filepath.Abs(dir)
 }
