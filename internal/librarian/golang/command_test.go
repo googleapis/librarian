2fe420d12c09ad3274f8cf4408f054260e52e546
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-05-29 13:31:15 -0400

    add unit tests

---
+++ internal/librarian/golang/command_test.go
@@ -0,0 +1,85 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package golang
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestMergeEnv(t *testing.T) {
+	for _, test := range []struct {
+		name    string
+		env     map[string]string
+		envPath string
+		want    func(base string) map[string]string
+	}{
+		{
+			name:    "nil env",
+			env:     nil,
+			envPath: "/original/path",
+			want: func(base string) map[string]string {
+				return map[string]string{
+					"PATH": filepath.Join(base, "bin") + ":/original/path",
+				}
+			},
+		},
+		{
+			name: "custom env keys merged",
+			env: map[string]string{
+				"FOO": "bar",
+			},
+			envPath: "/original/path",
+			want: func(base string) map[string]string {
+				return map[string]string{
+					"PATH": filepath.Join(base, "bin") + ":/original/path",
+					"FOO":  "bar",
+				}
+			},
+		},
+		{
+			name: "env overrides PATH",
+			env: map[string]string{
+				"PATH": "/env/custom/path",
+			},
+			envPath: "/original/path",
+			want: func(base string) map[string]string {
+				return map[string]string{
+					"PATH": "/env/custom/path",
+				}
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			baseDir := t.TempDir()
+			t.Setenv(envLibrarianDir, baseDir)
+			t.Setenv("PATH", test.envPath)
+			got, err := mergeEnv(test.env)
+			if err != nil {
+				t.Fatal(err)
+			}
+			wantAbsBase, err := filepath.Abs(baseDir)
+			if err != nil {
+				t.Fatal(err)
+			}
+			wantMap := test.want(wantAbsBase)
+			if diff := cmp.Diff(wantMap, got); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
