8aa57f091c7b0952256f3679b34fab96d810b5c2
yangyzs <171981480+yangyzs@users.noreply.github.com>
2026-06-10 20:20:49 -0400

    feat(internal/postprocessing): add copyFile function (#6364)
    Add the CopyFile function to the postprocessing package to
    wrap the filesystem copy operations, preparing for the YAML-based
    postprocessing workflow.
    
    Fixes #6295

---
+++ internal/postprocessing/fileops.go
@@ -0,0 +1,27 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package postprocessing provides tools for the YAML-based postprocessing workflow.
+package postprocessing
+
+import (
+	"github.com/googleapis/librarian/internal/filesystem"
+)
+
+// CopyFile copies a single file from the src path to the dst path.
+// It acts as a wrapper around filesystem.CopyFile to provide a unified
+// interface for all postprocessing file operations.
+func CopyFile(src, dst string) error {
+	return filesystem.CopyFile(src, dst)
+}
---
+++ internal/postprocessing/fileops_test.go
@@ -0,0 +1,56 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package postprocessing
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestCopyFile(t *testing.T) {
+	dir := t.TempDir()
+	srcPath := filepath.Join(dir, "src.txt")
+	dstPath := filepath.Join(dir, "dst.txt")
+	content := "hello copy"
+	if err := os.WriteFile(srcPath, []byte(content), 0644); err != nil {
+		t.Fatal(err)
+	}
+	if err := CopyFile(srcPath, dstPath); err != nil {
+		t.Fatal(err)
+	}
+	gotBytes, err := os.ReadFile(dstPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := string(gotBytes)
+	if diff := cmp.Diff(content, got); diff != "" {
+		t.Errorf("mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestCopyFile_Error(t *testing.T) {
+	dir := t.TempDir()
+	srcPath := filepath.Join(dir, "nonexistent.txt")
+	dstPath := filepath.Join(dir, "dst.txt")
+	err := CopyFile(srcPath, dstPath)
+	if !errors.Is(err, fs.ErrNotExist) {
+		t.Errorf("CopyFile() returned unexpected error: got %v, want %v", err, fs.ErrNotExist)
+	}
+}
