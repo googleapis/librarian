342661433d39606e013e5e784b13770cce14ed13
Carlos O'Ryan <coryan@google.com>
2026-06-16 22:40:45 +0000

    Gemini says, Carlos do

--- cmd/librarian/main.go
+++ cmd/librarian/main.go
@@ -25,7 +25,7 @@ import (
 func main() {
 	ctx := context.Background()
 	if err := librarian.Run(ctx, os.Args...); err != nil {
-		fmt.Fprintf(os.Stderr, "librarian: %v", err)
+		fmt.Fprintf(os.Stderr, "librarian: %v\n", err)
 		os.Exit(1)
 	}
 }
