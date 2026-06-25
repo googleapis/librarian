a6d950a157338083086d2b3d8c03150179628b10
yangyzs <171981480+yangyzs@users.noreply.github.com>
2026-06-24 23:00:05 -0400

    feat(internal/librarian/java): add decamelize utility for README generation (#6538)
    This utility splits CamelCase Java class names into space-separated
    words to generate human-readable titles for the README code samples table.
    
    For #6515

---
+++ internal/librarian/java/readme_test.go
@@ -0,0 +1,62 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package java
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestDecamelize(t *testing.T) {
+	for _, test := range []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "camel case",
+			input: "CamelCase",
+			want:  "Camel Case",
+		},
+		{
+			name:  "simple word",
+			input: "Word",
+			want:  "Word",
+		},
+		{
+			name:  "already separated",
+			input: "Camel Case",
+			want:  "Camel Case",
+		},
+		{
+			name:  "java acronym IamPolicy",
+			input: "IamPolicy",
+			want:  "Iam Policy",
+		},
+		{
+			name:  "java acronym GcsBucket",
+			input: "GcsBucket",
+			want:  "Gcs Bucket",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			got := decamelize(test.input)
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
