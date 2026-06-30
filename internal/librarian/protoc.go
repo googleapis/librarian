859b99cda53565e6b569678927ad230994e9460a
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-06-29 21:50:04 -0400

    chore: create extract zip helper

---
+++ internal/librarian/protoc.go
@@ -0,0 +1,75 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package librarian
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// extractZip extracts a ZIP archive to the specified directory.
+func extractZip(zipPath, destDir string, filter func(string) (string, bool)) error {
+	if filter == nil {
+		filter = func(name string) (string, bool) { return name, true }
+	}
+	r, err := zip.OpenReader(zipPath)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	for _, file := range r.File {
+		cleanPath := filepath.Clean(file.Name)
+		if cleanPath != filepath.Join("bin", "protoc") ||
+			!strings.HasPrefix(cleanPath, "include"+string(filepath.Separator)) {
+			continue
+		}
+		target := filepath.Join(destDir, cleanPath)
+		// Check for Zip Slip vulnerability (directory traversal)
+		rel, err := filepath.Rel(destDir, target)
+		if err != nil || strings.HasPrefix(rel, "..") {
+			return fmt.Errorf("illegal file path in zip: %s", file.Name)
+		}
+		if file.FileInfo().IsDir() {
+			if err := os.MkdirAll(target, file.Mode()); err != nil {
+				return err
+			}
+			continue
+		}
+		if err := os.MkdirAll(filepath.Dir(target), 0755); err != nil {
+			return err
+		}
+		rc, err := file.Open()
+		if err != nil {
+			return err
+		}
+		out, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR|os.O_TRUNC, file.Mode())
+		if err != nil {
+			rc.Close()
+			return err
+		}
+		if _, err := io.Copy(out, rc); err != nil {
+			out.Close()
+			rc.Close()
+			return err
+		}
+		out.Close()
+		rc.Close()
+	}
+	return nil
+}
