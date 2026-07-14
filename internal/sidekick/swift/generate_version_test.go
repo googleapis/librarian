54e04eef0e7862b2447f0b87a628e2398363659c
Carlos O'Ryan <coryan@google.com>
2026-07-14 11:20:41 -0400

    feat(sidekick/swift): generate core package versions
    In Swift, there is no programmatic way to obtain the version of the
    current package. We need to initialize a variable to the version.
    
    For GAPICs, it is easy to generate such a variable. For core libraries,
    we could maintain the version manually. That seems silly, librarian
    already knows how to bump versions, and librarian can mix generated and
    non-generated code.
    
    This PR introduces a new module type: `version` which only generates a
    version file. The types in the generated code are package-private. If
    needed, we can re-export them in the hand-crafted code.

---
+++ internal/sidekick/swift/generate_version_test.go
@@ -0,0 +1,54 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package swift
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/googleapis/librarian/internal/config"
+)
+
+func TestGenerateVersion(t *testing.T) {
+	library := &config.Library{
+		CopyrightYear: "2038",
+		Version:       "1.2.3-test",
+	}
+	outDir := t.TempDir()
+	if err := GenerateVersion(t.Context(), outDir, library); err != nil {
+		t.Fatal(err)
+	}
+	contents, err := os.ReadFile(filepath.Join(outDir, "Package+Version.swift"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	contentStr := string(contents)
+	for _, test := range []struct {
+		start string
+		want  string
+	}{
+		{"// Copyright ", "// Copyright 2038 Google LLC\n"},
+		{"static let version:", "static let version: String = \"1.2.3-test\"\n"},
+	} {
+		t.Run(test.start, func(t *testing.T) {
+			got := extractBlock(t, contentStr, test.start, "\n")
+			if diff := cmp.Diff(test.want, got); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s\n", diff)
+			}
+		})
+	}
+}
