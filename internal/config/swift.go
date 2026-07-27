03375a1eaec73239e0e0358fa14fa47730aa0a2f
Carlos O'Ryan <coryan@google.com>
2026-07-27 13:53:26 -0400

    Address review comments

--- internal/config/swift.go
+++ internal/config/swift.go
@@ -39,6 +39,10 @@ type SwiftPackage struct {
 	Modules []*SwiftModule `yaml:"modules,omitempty"`
 
 	// PackageNameOverride overrides the package name.
+	//
+	// This may be useful if the protobuf package lacks the necessary prefixes,
+	// e.g. `grafeas.v1` may be published as `google-grafeas-v1` to match the
+	// other packages.
 	PackageNameOverride string `yaml:"package_name_override,omitempty"`
 
 	// PerServiceTraits enables per-service compile-time flags.
