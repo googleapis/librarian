de99a1449a1dcfd0754a7e244a84a6cd32961050
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-08-03 16:28:10 -0400

    chore: parse proto package

--- internal/librarian/php/init.go
+++ internal/librarian/php/init.go
@@ -26,7 +26,30 @@ import (
 var (
 	namespaceRe     = regexp.MustCompile(`php_namespace\)?\s*=\s*"([^"]+)"`)
 	versionSuffixRe = regexp.MustCompile(`\\V\d+.*$`)
+	packageRe       = regexp.MustCompile(`^package\s+([^\s;]+);`)
 )
+
+type initParams struct {
+	componentName string
+	namespace     string
+	protoPackage  string
+}
+
+func newInitParams(googleapisDir, apiPath string) (*initParams, error) {
+	ns, err := namespace(googleapisDir, apiPath)
+	if err != nil {
+		return nil, err
+	}
+	pkg, err := protoPackage(googleapisDir, apiPath)
+	if err != nil {
+		return nil, err
+	}
+	return &initParams{
+		componentName: componentName(ns),
+		namespace:     ns,
+		protoPackage:  pkg,
+	}, nil
+}
 
 // namespace reads the php_namespace option from the first .proto file in the API directory.
 // If the option is not found, it generates a fallback namespace from the API path.
@@ -68,6 +91,30 @@ func componentName(namespace string) string {
 	}
 	comp := strings.TrimPrefix(namespace, `Google\`)
 	return strings.ReplaceAll(comp, `\`, "")
+}
+
+func protoPackage(googleapisDir, apiPath string) (string, error) {
+	file, err := searchForProto(googleapisDir, apiPath)
+	if err != nil {
+		return "", err
+	}
+	f, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		// Ignore comments.
+		if strings.HasPrefix(line, "//") {
+			continue
+		}
+		if matches := packageRe.FindStringSubmatch(line); len(matches) > 1 {
+			return versionSuffixRe.ReplaceAllLiteralString(matches[1], ""), nil
+		}
+	}
+	return "", scanner.Err()
 }
 
 // searchForProto finds the first .proto file in the API directory.
