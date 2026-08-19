4664b6a6f6daede8368525e61bbc895a05535b64
Sofia Leon <sofialeon@google.com>
2026-08-19 18:08:40 +0000

    style: gofmt php.go

--- tool/cmd/migrate/php.go
+++ tool/cmd/migrate/php.go
@@ -262,10 +262,10 @@ func findPHPLibraries(repoPath string, googleapisDir string, globalDefaultCommon
 		}
 		libraryName := php.DefaultLibraryName(apis[0].Path)
 		lib := &config.Library{
-			Name:          libraryName,
-			Version:       version,
-			APIs:          apis,
-			Output:        name,
+			Name:    libraryName,
+			Version: version,
+			APIs:    apis,
+			Output:  name,
 		}
 		derivedComp, err := php.ComponentNameForLibrary(googleapisDir, lib)
 		if err != nil {
