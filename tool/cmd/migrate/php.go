5470a1f24b0803f632414d87ff32341300ca4af3
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-08-27 13:40:04 -0400

    restore

--- tool/cmd/migrate/php.go
+++ tool/cmd/migrate/php.go
@@ -454,11 +454,6 @@ func specialCases(api *config.API) {
 			api.PHP = &config.PHPAPI{}
 		}
 		api.PHP.ExcludedProtos = append(api.PHP.ExcludedProtos, "google/rpc/http.proto")
-	case "google/cloud/dataform/v1", "google/cloud/dataform/v1beta1":
-		if api.PHP == nil {
-			api.PHP = &config.PHPAPI{}
-		}
-		api.PHP.AdditionalProtos = append(api.PHP.AdditionalProtos, "google/longrunning/operations.proto")
 	case "google/cloud/location":
 		if api.PHP == nil {
 			api.PHP = &config.PHPAPI{}
