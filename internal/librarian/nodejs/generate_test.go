0f27bd052dde71279f29c62385b0a14ebaaec0a0
JoeWang1127 <joewa@google.com>
2026-08-31 18:24:09 +0000

    comment

--- internal/librarian/nodejs/generate_test.go
+++ internal/librarian/nodejs/generate_test.go
@@ -994,6 +994,8 @@ func TestGenerate(t *testing.T) {
 	if testing.Short() {
 		t.Skip("slow test: Node.js code generation")
 	}
+	// Prepend the Librarian Node.js tools directory to PATH so testhelper.RequireCommand
+	// finds binaries installed by librarian install.
 	if binDir, err := getBinDir(); err == nil {
 		t.Setenv("PATH", binDir+string(os.PathListSeparator)+os.Getenv("PATH"))
 	}
