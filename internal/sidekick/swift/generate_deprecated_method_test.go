acced014d60efb97b686ea3bad4488e72f8b1974
Jeff Ching <chingor@google.com>
2026-09-16 17:53:38 +0000

    refactor(sidekick/swift): rename GoogleCloudAuth to GoogleAuth (#7597)
    Rename the GoogleCloudAuth module dependency and imports to GoogleAuth
    in the Swift code generator and templates.
    
    The swift-google-auth package uses GoogleAuth as its module name. Update
    the mustache templates, storage client generator, and tests to import
    and reference GoogleAuth.
    
    Towards https://github.com/googleapis/google-cloud-swift/issues/905

--- internal/sidekick/swift/generate_deprecated_method_test.go
+++ internal/sidekick/swift/generate_deprecated_method_test.go
@@ -189,7 +189,7 @@ func TestGenerateService_DeprecatedMethods(t *testing.T) {
 
 			swiftCfg := swiftConfig(t, []config.SwiftDependency{
 				{Name: "GoogleCloudGax", RequiredByServices: true},
-				{Name: "GoogleCloudAuth", RequiredByServices: true},
+				{Name: "GoogleAuth", RequiredByServices: true},
 				{ApiPackage: "google.longrunning", Name: "GoogleCloudLongrunningV1"},
 				{ApiPackage: "google.rpc", Name: "GoogleRpc"},
 			})
