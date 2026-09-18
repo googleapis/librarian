58a2f89272892665364f2e56783a156d8d73f5d8
JoeWang1127 <joewa@google.com>
2026-09-18 14:13:29 +0000

    rm unused err

--- internal/librarian/python/add.go
+++ internal/librarian/python/add.go
@@ -42,7 +42,6 @@ var (
 	errNewLibraryMustHaveOneAPI          = errors.New("a newly added library (in Python) must have exactly one API so that the default version can be populated")
 	errNewLibraryBadNamespace            = errors.New("derived GAPIC namespace would not match any approved namespace; consult with the Python team to determine whether the namespace should be approved, or whether GAPIC options should be specified for this API in librarian.yaml. See go/clientlibs-python-registered-namespaces for more details")
 	errExistingLibraryNoDefaultVersion   = errors.New("new APIs cannot be automatically added to a library without a default version")
-	errExistingLibraryCustomGAPICOptions = errors.New("new APIs cannot be automatically added to a library with custom GAPIC options")
 )
 
 // Add initializes a new Python library with default values.
