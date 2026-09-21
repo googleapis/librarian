11d97a452991f46783ebfba60fe6c61d6a9bf32e
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-09-21 10:34:39 -0400

    fix: set COREPACK_ENABLE_DOWNLOAD

--- internal/tool/pnpm/pnpm.go
+++ internal/tool/pnpm/pnpm.go
@@ -102,6 +102,7 @@ func env(binDir string) ([]string, error) {
 	env = append(env, "PNPM_CONFIG_GLOBAL_BIN_DIR="+binDir)
 	env = append(env, "PNPM_CONFIG_GLOBAL_DIR="+globalDir)
 	env = append(env, "PNPM_CONFIG_STORE_DIR="+storeDir)
+	env = append(env, "COREPACK_ENABLE_DOWNLOAD=0")
 	// TODO(https://github.com/googleapis/librarian/issues/6889): Remove legacy NPM_CONFIG_*
 	// environment variables once pnpm is upgraded to version 8+.
 	env = append(env, "NPM_CONFIG_GLOBAL_BIN_DIR="+binDir)
